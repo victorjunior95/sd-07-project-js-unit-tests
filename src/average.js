8bd3791c548712aa60fdb017045b06595e466f6d
Fernando <nando.neves@gmail.com>
2020-09-30 15:36:10 -0300

    codeclimate

--- src/average.js
+++ src/average.js
@@ -13,8 +13,9 @@
 */
 const average = (array) => {
   let soma = 0;
+  if (array.length === 0) return undefined;
   for (let i = 0; i < array.length; i += 1) {
-    if (typeof (array[i]) === 'number') {
+    if (typeof array[i] === 'number') {
       soma += Math.round(array[i]);
     } else {
       return undefined;
