8376e5d5e3a3bf6f1d624e25083fb455ff5fad59
TIAGO <tiagoesdrasvieira@gmail.com>
2020-09-30 19:54:16 -0300

    Desafio Obj Calculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -18,7 +18,15 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
-
-const calculator = {};
+const add = (number1, number2) => number1 + number2;
+const mult = (number1, number2) => number1 * number2;
+const div = (number1, number2) => Math.floor(number1 / number2);
+const sub = (number1, number2) => number1 - number2;
+const calculator = {
+  add,
+  mult,
+  div,
+  sub,
+};
 
 module.exports = calculator;
