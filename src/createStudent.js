33fd76ddf428b15ba839aea659a4f5174e5ebd8d
Fernando <nando.neves@gmail.com>
2020-09-30 16:53:10 -0300

    até createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,17 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let obj = {
+    name: nome,
+    feedback: frase = () => {
+      return `Eita pessoa boa!`
+    },
+  }
+ 
+
+  return obj
+};
+console.log (createStudent('Fernando'))
 
 module.exports = createStudent;
