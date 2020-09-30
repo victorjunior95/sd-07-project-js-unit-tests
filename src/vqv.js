7ec48daca0fbd8ec65f197f62635a9941e2e5378
Anderson Santana <anderslash@gmail.com>
2020-09-30 16:42:06 -0300

    Ajustes para o codeclimate aceitar

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,12 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome != 'string') return undefined;
-  if (typeof idade != 'number') return undefined;
+  if (typeof nome !== 'string') return undefined;
+  if (typeof idade !== 'number') return undefined;
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
+#VQV!`;
 };
 
 module.exports = vqv;
