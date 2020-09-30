2a8f6ce12f8be8686a00019676a0b11bc017b55c
Lfocosi <luccafocosi@gmail.com>
2020-09-30 16:57:24 -0300

    numbers

--- src/average.js
+++ src/average.js
@@ -15,13 +15,15 @@
 const average = (array) => {
   let resultado = 0;
   for (let numero = 0; numero < array.length; numero += 1) {
-    if (typeof (array[numero]) === 'number') {
-      resultado += array[numero];
-    } else {
+    if (typeof (array[numero]) !== 'number') {
       return undefined;
+    } else {
+      resultado += array[numero];
     }
   }
   return Math.round(resultado / 2);
 };
+
+console.log (average([' ']));
 
 module.exports = average;
