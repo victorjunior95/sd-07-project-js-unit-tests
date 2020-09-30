fc9038135cfd0ae7820be2a5a02c1945e29e65da
vitorAmorims <vtamorims@gmail.com>
2020-09-30 18:10:18 -0300

    inseridos testes na funçao numbers.

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -21,6 +21,7 @@ describe('#numbers', () => {
     assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
     assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
     assert.deepStrictEqual(numbers([1, 'a', 3]), false);
+    assert.deepStrictEqual(numbers([' ']), false);
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
