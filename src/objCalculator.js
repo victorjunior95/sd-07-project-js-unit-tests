1374ba4d3308638ce9e45719fd74560d58958b43
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-09-30 19:57:08 -0300

    fixing 7 requisition

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,9 +20,9 @@
 */
 
 const calculator = {
-  add: (num1, num2) => Math.trunc(num1 + num2),
-  mult: (num1,num2) => Math.trunc(num1 * num2),
-  div: (num1,num2) => Math.trunc(num1 / num2),
+  add: (num1, num2) => Math.trunc(num1 + num2), 
+  mult: (num1,num2) => Math.trunc(num1 * num2), 
+  div: (num1,num2) => Math.trunc(num1 / num2), 
   sub: (num1,num2) => Math.trunc(num1 - num2), 
 };
 
