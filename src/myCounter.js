68ff802e75b8dac980348d9648934d6c9a3a2bb5
marcelo ivan <sagacello@hotmail.com>
2020-09-30 20:37:49 -0300

    function myCounter corrigida!

--- src/myCounter.js
+++ src/myCounter.js
@@ -2,7 +2,7 @@
 /* eslint-disable block-scoped-var */
 /* eslint-disable no-redeclare */
 /* eslint-disable vars-on-top */
-
+/*eslint no-trailing-spaces: ["error", { "skipBlankLines": true }]*/
 /*
   A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
   Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
@@ -20,12 +20,12 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     // primeiro loop pega o 0 e vai para o segundo onde é
     // adicionado o 2 e o 3 e depois retorna para o 1
-    // o primeiro loop começa denovo so que agora do valor 
+    // o primeiro loop começa denovo so que agora do valor
     // de 1 e insere o 1 e vai para o segundo loop onde é inserido o 2 e o 3
     // o processo se repete ...
-    // foi colocado o const porque o array tem que ser imutavel e o 
-    // o counter2 deve ser definido porque se não o counter vai 
-    // ficar com os valores inicias de 0 até 3 como uma variavel unica ,
+    // foi colocado o const porque o array tem que ser imutavel e o
+    // o counter2 deve ser definido porque se não o counter vai
+    // ficar com os valores inicias de 0 até 3 como uma variavel unica,
     myArray.push(counter);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
       myArray.push(counter2);
