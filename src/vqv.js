e6f8202d716d6f85f3c8cef658e2d474df447ef6
cezar88augusto <cezar88augusto@gmail.com>
2020-09-30 16:44:42 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,20 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+
+  if (nome == undefined || idade == undefined) {
+    return undefined;
+  }
+
+  const string = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return string;
+}
+
+console.log(vqv("Tunico", 29));
 
 module.exports = vqv;
