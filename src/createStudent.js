db4756bb79d7ca41c75fc59c9d90f6f213c52bde
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-30 16:05:23 -0300

    requisito 5 feito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (string) => {
+  const obj = {
+    name: string,
+    feedback: () => 'Eita pessoa boa!'
+  };
+  return obj;
+};
 
 module.exports = createStudent;
