222b809f2d216365e1dccaee21c56aa65f23e38d
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-30 20:22:00 -0300

    Adicionando exercício createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (parameter) => {
+  const person = {
+    name: parameter,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return person;
+};
 module.exports = createStudent;
