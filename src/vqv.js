2d9a687a5708d4e8a6e58cd3cfbc0452b7c9df7a
Ana Karine <anakarine.ti@gmail.com>
2020-09-30 18:06:01 -0300

    3 - Implemente a função do arquivo src/vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const trybePhrase =
+  `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  } return trybePhrase;
+};
 
 module.exports = vqv;
