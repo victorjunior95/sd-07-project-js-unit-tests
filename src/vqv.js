8a5b6c20c713156015444e68c22dbd080cb25d23
Bruno <bruno_grb@hotmail.com>
2020-09-30 15:46:46 -0300

    funcao vqv finalizada e iniciando implementaçao do test circle.spec.js

--- src/vqv.js
+++ src/vqv.js
@@ -20,9 +20,9 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) return undefined;
   if (typeof (nome) !== 'string' || typeof (idade) !== 'number') return undefined;
   return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
