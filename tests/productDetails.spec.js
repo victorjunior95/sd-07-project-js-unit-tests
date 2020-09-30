754c6f989454e9569fb7bcd9813e6b446a868d2e
luc-zago <luc.zago@hotmail.com>
2020-09-30 18:30:32 -0300

    Cumpridos requisitos 5 e 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,38 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails, 'function');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual((productDetails('firstItem', 'secondItem').length), 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productDetails('firstItem', 'secondItem')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('firstItem', 'secondItem')[0], productDetails('firstItem', 'secondItem')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    
+    const firstProductIdEnd = () => {
+      const firstProductId = productDetails('firstItem', 'secondItem')[0].details.productId;
+      const stringToArray = firstProductId.split('');
+      const arrayLength = stringToArray.length;
+      let stringEnd = stringToArray.splice((arrayLength - 3), 3);
+      stringEnd = stringEnd.join('');
+      return stringEnd;
+    };
+
+    const secondProductIdEnd = () => {
+      const secondProductId = productDetails('firstItem', 'secondItem')[1].details.productId;
+      const stringToArray = secondProductId.split('');
+      const arrayLength = stringToArray.length;
+      let stringEnd = stringToArray.splice((arrayLength - 3), 3);
+      stringEnd = stringEnd.join('');
+      return stringEnd;
+    };
+
+    const stringFirstProductEnd = firstProductIdEnd();
+    const stringSecondProductEnd = secondProductIdEnd();
+
+    assert.strictEqual(stringFirstProductEnd, stringSecondProductEnd);
   });
 });
