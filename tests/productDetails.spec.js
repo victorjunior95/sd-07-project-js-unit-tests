1cef187ffc8cd94bdc85e54e478e59ab96b1e4dc
Carlos Souza <carlos.pessoal@hotmail.com>
2020-09-30 16:28:05 -0300

    Sexto requisito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,26 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(Object.entries(productDetails('Alcool gel', 'Máscara')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[0], 'object');
+    assert.deepStrictEqual(typeof productDetails('Alcool gel', 'Máscara')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const checkFinalCode = (obj) => {
+      let error = false;
+      for (const entrie in obj) {
+        if (obj[entrie].details.productId.split("123")[1] !== '') {
+          error = true;
+        }
+      }
+      return error;
+    }
+    assert.deepStrictEqual(checkFinalCode(productDetails('Alcool gel', 'Máscara')), false);
   });
 });
