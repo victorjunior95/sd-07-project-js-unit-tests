b447e91ca570a62e5a272ae53a0f71261db9b345
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-30 20:32:43 -0300

    8.myCounter.js

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,15 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterTwo = 2; counterTwo <= 3; counterTwo += 1) {
+      myArray.push(counterTwo);
     }
   }
   return myArray;
 };
+console.log(myCounter());
 
 module.exports = myCounter;
