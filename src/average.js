30f0d6b40245cdcc4cdf16f145983c2692f69c97
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-09-30 16:23:40 -0300

    Alteraçoes para o primeiro requisito

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    }
+    sum += array[i];
+  }
+  const averageNumber = Math.round(sum / array.length);
+  return averageNumber;
 };
 
 module.exports = average;
