301847762c548da3d9dd1b1b57da770eca61a07e
danielmlduarte <danielduarte.tryber@gmail.com>
2020-09-30 17:30:22 -0300

    Criando funçao para o requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createFeedback = () => 'Eita pessoa boa!';
+
+const createStudent = (name) => {
+  return {name: name, feedback: createFeedback};
+};
 
 module.exports = createStudent;
