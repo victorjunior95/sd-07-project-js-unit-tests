ee9e0c391705bc2e7bd3778a0ae2afee6fb3a4be
Muniz <vinicius.muniz.marin@gmail.com>
2020-09-30 16:46:23 -0300

    1.average.js

--- src/average.js
+++ src/average.js
@@ -1,17 +1,42 @@
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado. 
+  o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
 
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
     - average([2, 2]) // Retorno: 2;
-    - average([1, 2]) // Retorno: 1;
+    - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const undefinedAvarage = (array) => {
+  let notNumber = false;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      notNumber = true;
+    }
+  }
+  if (array.length === 0 || notNumber) {
+    return true;
+  }
+  return false;
+};
 
-const average = () => {};
+const average = (array) => {
+  if (undefinedAvarage(array)) {
+    return undefined;
+  }
+  let resultado = null;
+  let sum = null;
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
+  }
+  resultado = Math.round(sum / array.length);
+  return resultado;
+};
+
+console.log(average([1, '2']));
 
 module.exports = average;
