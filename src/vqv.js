992edef101fe816c81ab20763cff63904b02ac9c
Strongreen <strongreen@live.com>
2020-09-30 18:01:57 -0300

    exercicio 3 - vqv concluido

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,18 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if(nome !== undefined && idade !== undefined){
+    let text = `Oi, meu nome é ${nome}!\n` +
+    `Tenho ${idade} anos,\n` +
+    `trabalho na Trybe e mando muito em programação!\n` +
+    `#VQV!`
+
+    return text;
+  }
+
+  return undefined;
+
+};
 
 module.exports = vqv;
