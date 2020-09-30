6de353224c6b7f8f4ea5e7a60f3c8d76d3f0d74a
renatomak <renato.mark.silva@gmail.com>
2020-09-30 18:03:09 -0300

    funçao createStudent.js-CC

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,10 +18,8 @@
 const createStudent = (string) => {
   return {
     name: string,
-    feedback:() => {
-      return 'Eita pessoa boa!';
-    }
-  }
+    feedback: () => 'Eita pessoa boa!',
+  };
 };
 
 module.exports = createStudent;
