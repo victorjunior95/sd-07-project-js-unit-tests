5096acb601b127a423331a3208d4476e74d931c5
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-30 12:51:38 -0300

    3rd requirement done

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (student) => {
+  return (studentObject = {
+    name: student,
+    feedback: () => {
+      return 'Eita pessoa boa!';
+    },
+  });
+};
 
 module.exports = createStudent;
