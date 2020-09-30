a0cd8592ac3a1f5d30ff054a22836f1f37cf6a87
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-30 19:15:31 -0300

    Requisito 5: function myCounter()

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let j = 2; j <= 3; j += 1) {
+      myArray.push(j);
     }
   }
   return myArray;
