4ecdd8d988e7a5a7693dd150745624bef6699040
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-09-30 17:04:58 -0300

    Refatorando requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
