529ba71937e048a0c77686338a9cb90b63b6976e
ThiagoPederzolli <thiagopederzolli@gmail.com>
2020-09-30 18:26:33 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (string) => {
+  const name = string;
+  return {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+};
+
 
 module.exports = createStudent;
