bb4c7d453b5ee99576d4c5270771ea76d0102236
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-30 17:08:19 -0300

    createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = name => {
-  return {
+const createStudent = (name) => {
+  const student = {
     name,
     feedback: () => 'Eita pessoa boa!',
   };
+
+  return student;
 };
 
 module.exports = createStudent;
