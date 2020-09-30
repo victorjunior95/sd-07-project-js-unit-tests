7646e7b8e4f2495d098f25a1afa127a975b88f25
Jorge Alves <alvesdjorge@gmail.com>
2020-09-30 18:59:52 -0300

    func average

--- src/average.js
+++ src/average.js
@@ -8,12 +8,56 @@
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
     - average([2, 2]) // Retorno: 2;
-    - average([1, 2]) // Retorno: 1;
+    - average([1, 2]) // Rvtorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-   // add your implementation here
-};
+const average = (arr) => {
+     // add your implementation here
+     let sum = 0;
+     let media = 0;
+     let arrString = []
 
-module.exports = average;
+     if (arr.length == 0) { 
+       return undefined; 
+     } else {
+      for (let i = 0; i < arr.length; i += 1) {
+        if (typeof arr[i] !== 'number') {
+          return undefined;
+        } else {
+          sum =+ arr[i];
+        }
+      }
+      return Math.round(sum / (arr.length));
+     };
+};
+//      for (let i = 0; i < arr.length; i += 1) {
+//       if (typeof arr[i] !== 'number') {
+//         arrString.push(arr[i])
+//       } else {
+//         cont  =+ array[i];
+//       }
+//     }
+//     media = cont / arr.length
+//     if (arrString.length == 0) {
+//       return cont;
+      
+//     } else {
+//      return undefined;
+//     }
+// };
+
+
+// const average = (array) => {
+//   let sum = 0;
+
+//   if (array.length === 0) { return undefined; }
+//   for (let index = 0; index < array.length; index += 1) {
+//     if (typeof array[index] !== 'number') {
+//       return undefined;
+//     }
+//     sum = array[index] + sum;
+//   }
+//   return Math.round(sum / (array.length));
+// };
+
