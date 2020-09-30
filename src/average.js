9aaf2373e8db85979aa19fe1145af7c00bc7e23b
Renato Silva <renatosdev@gmail.com>
2020-09-30 16:22:48 -0300

    Average.js - Correcao

--- src/average.js
+++ src/average.js
@@ -13,16 +13,16 @@
 */
 
 const average = (array) => {
-  if (array.length == 0) {
+  if (array.length === 0) {
     return undefined;
   }
   let sumArray = 0;
 
-  for (let index = 1; index < array.length; index += 1) {
-    if (typeof array !== 'number') {
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
       return undefined;
     }
-    sumArray += index;
+    sumArray += array[index];
   }
   const mean = Math.round(sumArray / array.length);
   return mean;
