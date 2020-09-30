c1244d97248fe45a33dc7bc9240c582a79cbc6e2
Mauro Junqueira <maurojunqueira@pop-os.localdomain>
2020-09-30 18:33:25 -0300

    Corrigindo o climate3

--- src/average.js
+++ src/average.js
@@ -12,19 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (input) => { 
+const average = (input) => {
   let total = 0;
-  if(input.length === 0){
-     return undefined;
-   }
+  if (input.length === 0) {
+    return undefined;
+  }
   for (index = 0; index < input.length; index += 1) {
     if (typeof (input[index]) === 'number') {
       total += (input[index]);
     } else {
       return undefined;
-    }    
+    }
   }
   return Math.round(total / input.length);
 }
-const nums = [1, 2, 3, '4', 5];
 module.exports = average;
