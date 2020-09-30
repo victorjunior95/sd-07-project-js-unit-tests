0b03142fc71a53fb6f4f8640b31bcbecebd7f795
Rodrigo M Sudario <monteiro.sudario@gmail.com>
2020-09-30 18:41:05 -0300

    corrigindo issues

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,8 +18,8 @@
 const createStudent = (nome) => {
   const estudante = {
     name: nome,
-    feedback: () => 'Eita pessoa boa!'
-  }
+    feedback: () => 'Eita pessoa boa!',
+  };
   return estudante;
 };
 
