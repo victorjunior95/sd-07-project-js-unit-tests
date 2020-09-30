441653432f28d5f5250bac74da3df1a8ea8306ed
vitorAmorims <vtamorims@gmail.com>
2020-09-30 18:57:50 -0300

    corrigido condiçao para funçao sem parametros

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let msgReturn;
+  if (nome == null || idade == null) {
+    msgReturn = undefined;
+  } else {
+    msgReturn = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n`+'#VQV!'
+  }
+  return msgReturn;
+};
 
 module.exports = vqv;
