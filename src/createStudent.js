efec65b18603221bad45aca8bd48e78350aa5ee7
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-30 16:46:28 -0300

    Requisito 04 e 05

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  resultObject = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return resultObject;
+};
 
 module.exports = createStudent;
