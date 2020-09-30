04863abdaaef6ea48d3b2600f459841527c4b8fb
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-30 15:18:06 -0300

    commit inicial e requisito 1, por algum motivo um tal de package-lock.json foi modificado...

--- src/average.js
+++ src/average.js
@@ -12,6 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let result = 0;
+  if (array.length === 0) return undefined;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    result += array[index];
+  }
+  result /= array.length;
+  return Math.round(result);
+};
 
 module.exports = average;
