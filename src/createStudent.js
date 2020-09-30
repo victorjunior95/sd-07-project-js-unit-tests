a06697112a875b6a30ab7beb5fe49decbedc7144
Danwhat <daanwhat@gmail.com>
2020-09-30 17:06:33 -0300

    circle

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  newObj = {}
+  newObj.name = name
+  newObj.feedback = 'Eita pessoa boa!'
+  return newObj
+};
 
+console.log(newObj)
 module.exports = createStudent;
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,20 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.deepStrictEqual(typeof circle(1), 'object')
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(Object.keys(circle(1)).length, 3)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.deepStrictEqual(circle(), undefined)
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.deepStrictEqual(circle(2).radius, 2)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.equal(circle(3).area, 28.26)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84})
   });
 });
+
+//console.log(Object.keys(circle(1)).length)
