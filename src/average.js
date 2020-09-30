d8077e9df5b1f751c58a388dd97f424a050ebdda
murilopereira006 <muriloedu006@gmail.com>
2020-09-30 17:33:47 -0300

    comecando o projeto

--- src/average.js
+++ src/average.js
@@ -13,11 +13,14 @@
 */
 
 const average = (array) => {
-  let sum = 0
+  let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return 'undefined';
+    }
     sum = array[index] + sum;
   }
-  return sum/(array.length + 1);
+  return sum / (array.length + 1);
 };
 
 module.exports = average;
