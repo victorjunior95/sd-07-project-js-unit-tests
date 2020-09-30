8b1a7ad1feeb2be4ad4d85e217e1ea08efbfebfe
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2020-09-30 19:36:49 -0300

    myCounter

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,13 +30,16 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+
     assert.strictEqual(Object.values(circle(2)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+
     assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // assert.strictEqual(parseFloat((circle(3).area).toPrecision(4)), 28.26);
+    assert.strictEqual(parseFloat((circle(3).area).toPrecision(4)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), {area: 28.26, circumference: 18.84, radius: 3});
   });
