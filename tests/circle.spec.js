adb6897e7822f8df1684f2c05e66b80ee30b2231
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-09-30 19:45:37 -0300

    outra tentativa do 4 requisito

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,11 +31,11 @@ describe('#circle', () => {
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference,12.56);
     assert.strictEqual(parseFloat((circle(3).area).toPrecision(4)), 28.26);
-    assert.strictEqual(circle(3),{ 
+    assert.deepStrictEqual(circle(3),{ 
       radius: 3,
       area: 28.259999999999998,
       circumference: 18.84 });
-      
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
