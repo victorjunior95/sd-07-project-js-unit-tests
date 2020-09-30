91ea864f0423c0dedde80646c5ce98f1b3fec7b0
Rodrigo <rd64103@gmail.com>
2020-09-30 17:58:30 -0300

    vqv climate

--- src/vqv.js
+++ src/vqv.js
@@ -17,11 +17,12 @@
 */
 
 const vqv = (nome, idade) => {
-if (nome === undefined || idade === undefined) return undefined
-return `Oi, meu nome é ${nome}!\n` + 
-`Tenho ${idade} anos,\n` + 
-`trabalho na Trybe e mando muito em programação!\n` + 
-`#VQV!`};
-console.log(vqv());
+  if (nome === undefined || idade === undefined) return undefined;
+  return `Oi, meu nome é ${nome}!\n` +
+  `Tenho ${idade} anos,\n` +
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!'
+};
+
 
 module.exports = vqv;
