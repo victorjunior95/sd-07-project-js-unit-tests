fbe6f44ef6bab3d069bca3bd03fb3710dcf86989
Berilo Wanderley <wberilo@gmail.com>
2020-09-30 15:03:41 -0300

    cc

--- src/average.js
+++ src/average.js
@@ -14,7 +14,7 @@
 
 const average = (array) => {
   let sum = 0;
-  for (let i in array) {
+  for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
     }
