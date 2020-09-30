5f07e185c3d17c4cfcf3686e5ead7753a71f0f6a
renatomak <renato.mark.silva@gmail.com>
2020-09-30 17:13:58 -0300

    Funçao vqv - CC

--- src/vqv.js
+++ src/vqv.js
@@ -17,11 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-  let exitMsn = undefined;
-  if (typeof nome !== 'string' || typeof idade !== 'number'){
+  let exitMsn;
+  if (typeof nome !== 'string' || typeof idade !== 'number') {
     return undefined;
   } 
-
   exitMsn = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
