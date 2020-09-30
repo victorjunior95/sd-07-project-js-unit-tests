419af48a474387db637ad9f96c8e26f9c9932f37
Berilo Wanderley <wberilo@gmail.com>
2020-09-30 15:09:02 -0300

    fixes

--- src/average.js
+++ src/average.js
@@ -14,8 +14,11 @@
 
 const average = (array) => {
   let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
   for (let i = 0; i < array.length; i += 1) {
-    if (typeof array[i] !== 'number') {
+    if (typeof array[i] != 'number') {
       return undefined;
     }
     sum += array[i];
