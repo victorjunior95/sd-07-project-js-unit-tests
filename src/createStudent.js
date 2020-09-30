c17170fd67f8b281604752bf744626e138b103d2
Melissa Gomes <mel.ge171201@gmail.com>
2020-09-30 18:04:01 -0300

    tesando requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  return {
-    name: nome,
-    feedback: () => 'Eita pessoa boa!'
-  }
-};
+const createStudent = nome => ({
+  name: nome,
+  feedback: () => 'Eita pessoa boa!',
+});
+
 
 module.exports = createStudent;
