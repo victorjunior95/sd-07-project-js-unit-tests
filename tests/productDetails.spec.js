f9243d15c25096310e8642d26d5b73c1163a1bc6
Pablo Pessanha <pablopessanh@gmail.com>
2020-09-30 16:13:00 -0300

    6th requirement done

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,20 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const products = productDetails('Arroz', 'Feijão');
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof products, 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(products.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof products, 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(products[0], products[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    products.forEach((product) => {
+      const arrProduct = product.details.productId;
+      assert.deepStrictEqual(arrProduct.endsWith('123'), true);
+    });
   });
 });
