101a89aa56613fe927b31d85792e8f8f7ff1723d
Emanuelle Brasil <emanuelle.rbl@gmail.com>
2020-09-30 19:19:34 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const person = {
+    name: nome,
+    feedback: () => ('Eita pessoa boa!')  
+  };
+  return person;
+};
 
 module.exports = createStudent;
