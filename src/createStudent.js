60519085bb326fed59fbf39b1e4ce6868124fbcc
brendalima <brenda.c.lima@poli.ufrj.br>
2020-09-30 18:25:01 -0300

    5th requirement, creating function that creates an object

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const student = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
 
+const test = createStudent('Brenda');
+console.log(test);
 module.exports = createStudent;
