e091235daf855ed03666d593cab43eeb3f8a6d2c
Francisco <franciscoliratorres@gmail.com>
2020-09-30 18:32:16 -0300

    Add parte 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const frase = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return frase;
+};
 
 module.exports = createStudent;
