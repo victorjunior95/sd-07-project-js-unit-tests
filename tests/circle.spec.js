a74fa1881e15190425db288f93a318573f3d6742
Julio Kauer <juliokauer@gmail.com>
2020-09-30 17:04:36 -0300

    Corrigindo os problemas do arredondamento do circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,8 +35,10 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).area, 28.259999999999998);
+    assert.strictEqual(parseFloat(circle(3).area.toFixed(2)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
+    let myCircle = circle(3);
+    myCircle.area = parseFloat(circle(3).area.toFixed(2));
+    assert.deepStrictEqual(myCircle, { radius: 3, area: 28.26, circumference: 18.84 });
   });
 });
