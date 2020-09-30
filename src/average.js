3993c636b19017dd64354c5ced564fffc7ada646
André Ferreira <andrefpires1@gmail.com>
2020-09-30 17:22:06 -0300

    Função average

--- src/average.js
+++ src/average.js
@@ -12,7 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-// Começando projeto vqv
+const average = (arr) => {
+  let sum = 0;
+  let answer;
+  for(let i = 0; i < arr.length; i += 1) {
+    if(arr !== [] && typeof(arr[i]) === 'number') {
+      sum += arr[i];
+      answer = Math.round(sum / arr.length);
+    } else {
+      return undefined;
+    }
+  }
+  return resposta;
+};
 
 module.exports = average;
