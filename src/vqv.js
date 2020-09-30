261f46eb72ec243f722be35a5bc3f8149a751542
Lfocosi <luccafocosi@gmail.com>
2020-09-30 18:03:43 -0300

    vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let string = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`
+  
+  return string;
+};
 
 module.exports = vqv;
