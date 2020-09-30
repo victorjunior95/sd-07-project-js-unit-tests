a800bb2f4ca70bda76dd281c388582cfd29e05b8
Julio Kauer <juliokauer@gmail.com>
2020-09-30 16:19:54 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const object = {};
+  object.name = name;
+  object.feedback = () => 'Eita pessoa boa!';
+  return object;
+};
 
 module.exports = createStudent;
