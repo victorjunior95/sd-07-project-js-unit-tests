626ffad4c247bd38a88306183e1cb896e9d976d0
Francisco <franciscoliratorres@gmail.com>
2020-09-30 16:11:31 -0300

    Add parte 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (arr) => {
+  let sum = 0;
+  if (arr.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== 'number') {
+      return undefined;
+    }
+    sum += (arr[i]);
+  }
+  const output = (sum / arr.length);
+  return Math.round(output);
 };
 
 module.exports = average;
