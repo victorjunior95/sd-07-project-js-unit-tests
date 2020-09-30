fcd32aede4998f38c8668c03454444ddfedfe877
Elano Evaristo <elano3004@gmail.com>
2020-09-30 18:05:36 -0300

    resolucao das questoes do projeto :rocket:

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const circle = require('../src/circle');
+const assert = require("assert");
+const circle = require("../src/circle");
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -23,15 +23,24 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#circle', () => {
-  it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+describe("#circle", () => {
+  it("given a radius, should return an object with circles info", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(2), "object");
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.keys(circle(2)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2)["circumference"], 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(Number(circle(3)["area"].toFixed(2)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84,
+    });
   });
 });
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len*/
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -31,14 +31,35 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#productDetails', () => {
-  it('tests the function has the correct behaviour', () => {
-    assert.fail();
+describe("#productDetails", () => {
+  it("tests the function has the correct behaviour", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails(), "object");
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(
+      typeof productDetails("Alcool gel", "Máscara")[0],
+      "object",
+    );
+    assert.strictEqual(
+      typeof productDetails("Alcool gel", "Máscara")[1],
+      "object",
+    );
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(
+      productDetails("Alcool gel", "Máscara")[0],
+      productDetails("Alcool gel", "Máscara")[1],
+    );
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(
+      productDetails("Alcool gel", "Máscara")[0]["details"][
+        "productId"
+      ].includes("123"),
+      productDetails("Alcool gel", "Máscara")[1]["details"][
+        "productId"
+      ].includes("123"),
+    );
   });
 });
