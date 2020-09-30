dfdb40bfa716acacfa955dea32c51471945f3f57
Felipe Nascimento <flpnascto@gmail.com>
2020-09-30 20:15:26 -0300

    Verificando requisito 8. Verificações do CodeClimate.

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterAux = 2; counterAux <= 3; counterAux += 1) {
+      myArray.push(counterAux);
     }
   }
   return myArray;
