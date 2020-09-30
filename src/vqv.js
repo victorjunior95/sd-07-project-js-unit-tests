6de8950e77c5ecaddaba7470d6ba87e8e0ce09ca
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-09-30 18:47:24 -0300

    Requirement 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,6 +20,9 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  return (`Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV!`);
+  return (`Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`);
 };
 module.exports = vqv;
