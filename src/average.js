71abbf505a1ce93b6230da5795394f21cc2459ad
Rodrigo <rd64103@gmail.com>
2020-09-30 17:17:37 -0300

    feito os circulos e climate

--- src/average.js
+++ src/average.js
@@ -13,13 +13,13 @@
 */
 
 const average = (array) => {
-  let sum = 0
+  let sum = 0;
   if (array.length === 0) return undefined
   for (let i = 0; i < array.length; i += 1) {
-    if (typeof(array[i]) !== "number") return undefined
-    sum += array[i]
+    if (typeof (array[i]) !== 'number') return undefined;
+    sum += array[i];
   }
-  return Math.round(sum/array.length)
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
