4a39368e5695ca450fd294814cd12e6afc346b73
marcelo ivan <sagacello@hotmail.com>
2020-09-30 20:34:48 -0300

    function myCounter corrigida

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,13 +18,17 @@
 const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
-    // primeiro loop pega o 0 e vai para o segundo onde é adicionado o 2 e o 3 e depois retorna para o 1 
-    // o primeiro loop começa denovo so que agora do valor de 1 e insere o 1 e vai para o segundo loop onde é inserido o 2 e o 3
+    // primeiro loop pega o 0 e vai para o segundo onde é
+    // adicionado o 2 e o 3 e depois retorna para o 1
+    // o primeiro loop começa denovo so que agora do valor 
+    // de 1 e insere o 1 e vai para o segundo loop onde é inserido o 2 e o 3
     // o processo se repete ...
-    // foi colocado o consto porque o array tem que ser imutavel e o let não vai permitir o escopo se quebrar e a variavel ser substituida ao inves de adicioanda
+    // foi colocado o const porque o array tem que ser imutavel e o 
+    // o counter2 deve ser definido porque se não o counter vai 
+    // ficar com os valores inicias de 0 até 3 como uma variavel unica ,
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
