df7dd3448c377d10d7a6203ee5a63366d1fba18b
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-09-30 19:00:10 -0300

    Corrigindo teste circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -36,8 +36,18 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).area, 28.26);
+    assert.strictEqual(parseFloat((circle(3).area).toPrecision(4)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
+    const out = {}
+    out.radius = parseFloat(circle(3).radius.toPrecision(4));
+    out.area = parseFloat(circle(3).area.toPrecision(4));
+    out.circumference = parseFloat(circle(3).circumference.toPrecision(4));
+
+    const expected = {
+      radius: 3,
+      area: 28.26,
+      circumference: 18.84,
+    };
+    assert.deepStrictEqual(out, expected);
   });
 });
