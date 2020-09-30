e80666f5c4ea36ed815e8b7dccd14343e5acd019
Fernando Antunes <fernando.antunes1@gmail.com>
2020-09-30 19:21:38 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,8 +17,8 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  for (var counter1 = 0; counter1 <= 3; counter1 += 1) {
+    myArray.push(counter1);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
@@ -27,3 +27,5 @@ const myCounter = () => {
 };
 
 module.exports = myCounter;
+
+console.log(myCounter());
