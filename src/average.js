fd610dadbda7016c735d87a145b37828828e0baf
ericvini <ericcobain3@gmail.com>
2020-09-30 20:18:15 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,9 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (array) => {
+  let soma = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
 
-  //
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'number') {
+      soma += array[index];
+    } else {
+      return undefined;
+    }
+  }
+
+  return Math.round(soma / array.length);
 };
+
+average([]);
 
 module.exports = average;
