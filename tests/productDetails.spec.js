deefde8f3c8fe085739b8dc59e48fb8d530d5acb
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-09-30 19:11:55 -0300

    requisito 6, lógica para testar se o dois itens da array eram objetos estava incorreta, consertei o problema

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -41,14 +41,18 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(forShort.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof Object.keys(forShort), 'object');
-    // Teste que os dois objetos são diferentes entre si.
-    assert.notDeepStrictEqual(forShort[0], forShort[1])
-    // (Difícil) Teste que os dois productIds terminam com 123.
     let control;
-    if (forShort[0].details['productId'].endsWith('123') && forShort[1].details['productId'].endsWith('123')) {
+    if (typeof forShort[0] === 'object' && typeof forShort[1] === 'object'){
       control = true;
     }
     assert.strictEqual(control, true);
+    // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(forShort[0], forShort[1])
+    // (Difícil) Teste que os dois productIds terminam com 123.
+    let control2;
+    if (forShort[0].details['productId'].endsWith('123') && forShort[1].details['productId'].endsWith('123')) {
+      control2 = true;
+    }
+    assert.strictEqual(control2, true);
   });
 });
