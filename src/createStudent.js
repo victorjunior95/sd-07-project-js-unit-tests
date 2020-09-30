4a7fd9dbc37fea19bcccc6714f1f3b48cf2e53a7
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-30 16:54:04 -0300

    Corrigindo issues code climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,8 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
-  return { name: name, feedback: () => 'Eita pessoa boa!' };
-};
+const createStudent = nome => ({ name: nome, feedback: () => 'Eita pessoa boa!' });
 
 module.exports = createStudent;
