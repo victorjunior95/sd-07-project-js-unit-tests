079dfb53e9c7b83203f902bac2f5b11de3da4cd3
Fernando <nando.neves@gmail.com>
2020-09-30 17:55:20 -0300

    até calculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -1,9 +1,6 @@
 /*
   Desenvolva um objeto calculator que possui quatro chaves:
-    - add;
-    - mult;
-    - div;
-    - sub.
+    -
   Para cada uma delas atribua uma função que realiza a respectiva operação.
   A função deve receber dois inteiros e retornar um inteiro.
   Os resultados das divisões devem sempre ser arredondados para baixo.
@@ -19,6 +16,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: soma = (a, b) => a + b,
+  mult: mul = (a, b) => a * b,
+  div: divi = (a, b) => Math.floor(a / b),
+  sub: menos = (a, b) => a - b,
+};
 
 module.exports = calculator;
