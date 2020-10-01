a3c28d4080b3e46fe87960e8252f288d670479ee
danielmlduarte <danielduarte.tryber@gmail.com>
2020-10-01 00:59:53 -0300

    Criando testes unitarios para o requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,22 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual((typeof productDetails()[0], typeof productDetails()[1]), ('object', 'object'));
     // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    const itemOne = productDetails('Alcool gel', 'Máscara')[0];
+    const itemTwo = productDetails('Alcool gel', 'Máscara')[1];
+    for (let index in itemOne) {
+      assert.notStrictEqual(itemOne[index], itemTwo[index]);
+    }
+    // (Difícil) Teste que os dois productIds terminam com 123.    
+    for (let index = 0; index < productDetails().length; index += 1) {
+      assert.strictEqual(productDetails()[index].details.productId.endsWith('123'), true);
+    }
   });
 });
