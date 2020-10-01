314c39cca3680f6489e3901bdbeb4609b3e3772f
Mauro Junqueira <maurojunqueira@pop-os.localdomain>
2020-10-01 10:45:16 -0300

    Finalizado requisito 3 agora vai

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,10 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  const phrase = ' Oi, meu nome é ' + nome + ' !\n Tenho ' + idade + ' anos,\n  trabalho na Trybe e mando muito em programação!\n  #VQV! ';
+  const phrase = 'Oi, meu nome é '+ nome +'!\n' +
+  'Tenho ' + idade + ' anos,\n' +
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!';
   return phrase;
 };
 
