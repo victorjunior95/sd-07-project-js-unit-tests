17936524664b3fec9e0f488912e34895857188fb
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-09-30 21:20:29 -0300

    Criado função para criação de objetos de estudante

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudante = {
+    name: nome,
+    feedback: () => {
+      return 'Eita pessoa boa!';
+    }
+  }
+  return estudante;
+};
 
 module.exports = createStudent;
