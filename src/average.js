ef0e94111604da85143b9ff95cb9adada8efeaa1
Galaraz <jvictor.si.2013@gmail.com>
2020-10-01 17:43:22 -0300

    pela ciencia 4

--- src/average.js
+++ src/average.js
@@ -13,15 +13,15 @@
 */
 
 const average = (array) => {
-let element=0;
+let element = 0;
 if (array.length === 0){
   return undefined;
 }
 for (let index = 0; index < array.length; index +=1) {
-  if(typeof (arry[index]) === 'number'){
+  if (typeof (arry[index]) === 'number') {
     element += array[index];
   } 
-  else{
+  else {
     return undefined;
   }
  }
