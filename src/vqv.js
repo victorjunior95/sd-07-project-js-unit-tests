d2949e442c306b3e480eba26b7e0ca0834143fa1
Galaraz <jvictor.si.2013@gmail.com>
2020-10-01 19:05:59 -0300

    xeroki gomes

--- src/vqv.js
+++ src/vqv.js
@@ -19,7 +19,7 @@
 const vqv = (nome, idade) => {
   if (nome == null && idade == null) {
     return undefined;
-  } return `Oi, meu nome é ${nome}!
+  } return ` Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
