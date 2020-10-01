281543495d5cf9a7e21d5fed6b5660aebb6a172c
Lfocosi <luccafocosi@gmail.com>
2020-10-01 11:29:10 -0300

    cloud climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => { return { feedback() { return 'Eita pessoa boa!'; }, name: nome }; };
+const createStudent = (nome) => ({ feedback() { return 'Eita pessoa boa!'; }, name: nome });
 
 module.exports = createStudent;
