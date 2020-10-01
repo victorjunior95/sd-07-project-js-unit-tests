e27f6f1be57741d2816f80a1cd4230454eb141b4
Demistocles <demi.treze@gmail.com>
2020-10-01 20:07:37 -0300

    enviando o primeiro requisito

--- src/average.js
+++ src/average.js
@@ -12,8 +12,25 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  //coment
+const average = (number) => {
+  if (number.length === 0) {
+    return undefined;
+  }
+
+  for (index = 0; index < number.length; index +=1) {
+    if (typeof(number[index]) !== 'number')
+      return undefined;
+  }
+
+  let result
+  let sumArray = 0
+
+  for (index = 0; index < number.length; index +=1) {
+    sumArray += number[index];
+  } 
+  
+  result = Math.round(sumArray / number.length)
+  return result 
 };
 
 module.exports = average;
