866b10eeabbfa8fe92eba4e2bd83aa20d8b1c3b3
moacyrrln <moacyrrln@gmail.com>
2020-10-01 15:25:09 -0300

    1525

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -40,9 +40,9 @@ describe('#circle', () => {
     assert.deepStrictEqual(circle(2), { radius: 2, area: 12.56, circumference: 12.56 });
 
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).area, 28.26);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
 
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
   });
 });
