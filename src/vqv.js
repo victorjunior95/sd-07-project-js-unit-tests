7f51f51db3d5f8bcecf7ca8c3db4af6304fa7a3f
Mauro Junqueira <maurojunqueira@pop-os.localdomain>
2020-10-01 11:27:36 -0300

    VQV com tudo na mesma linha

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,7 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
