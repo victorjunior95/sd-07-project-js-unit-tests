c696bef62c0a97f96041595dd0a87d9285d03e4e
Israel Menon <zezomenon@gmail.com>
2020-10-01 16:34:31 -0300

    Requisito 5, criando função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameStudent) => {
+  const student = {
+    name: nameStudent,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
 
 module.exports = createStudent;
