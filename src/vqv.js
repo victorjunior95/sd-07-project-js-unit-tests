f97125e09e48b1a498f747d8f761feeccbe6fb40
Rogerio-feitor <roger.feitor@gmail.com>
2020-10-01 20:01:59 -0300

    Corrigindo os erros do codeclimate e os erros da função #VQV Trybe.

--- src/vqv.js
+++ src/vqv.js
@@ -20,9 +20,10 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}! \n
-     Tenho ${idade} anos, \n` +
+  return `  Oi, meu nome é ${nome}!
+Tenho ${idade} anos, \n` +
      'trabalho na Trybe e mando muito em programação! \n' +
      '#VQV!';
 };
 module.exports = vqv;
+
