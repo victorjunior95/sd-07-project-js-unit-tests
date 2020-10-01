2737305cfe01562a36dde275c8622fb371906f69
Galaraz <jvictor.si.2013@gmail.com>
2020-10-01 17:53:59 -0300

    ta indentado  serio deixa eu

--- src/average.js
+++ src/average.js
@@ -11,22 +11,20 @@
     - average([1, 2]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
 const average = (array) => {
-let element = 0;
-if (array.length === 0){
-  return undefined;
-};
-for (let index = 0; index < array.length; index +=1) {
-  if (typeof (arry[index]) === 'number') {
-    element += array[index];
-  } 
-  else {
+  let element = 0;
+  if (array.length === 0) {
     return undefined;
-  };
- };
- const rounds = Math.round(element/array.length);
- return rounds;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (arry[index]) === 'number') {
+      element += array[index];
+    } else {
+      return undefined;
+    }
+  }
+  const rounds = Math.round(element / array.length);
+  return rounds;
 };
 
 module.exports = average;
