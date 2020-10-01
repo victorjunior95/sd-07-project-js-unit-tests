9bfca99709c29a9870584ccf11f9286cb3225d2a
Danwhat <daanwhat@gmail.com>
2020-10-01 17:46:28 -0300

    faltou um

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -59,8 +59,9 @@ describe('#createMenu', () => {
 
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
 
-    result = 'objetoQualquer';
-    expected = 'objetoQualquer';
+    objQualquer = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }
+    result = createMenu(objQualquer).fetchMenu();
+    expected = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };
     assert.deepStrictEqual(result, expected);
 
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
