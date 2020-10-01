6447d7507979e72a4665491cf816ab295316eaad
vitorAmorims <vtamorims@gmail.com>
2020-10-01 13:07:19 -0300

    teste que os dois itens dentro do array retornado pela funçao sao objetos.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -39,6 +39,7 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(Object.keys(productDetails()).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(productDetails(), Object.values(productDetails()));
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
