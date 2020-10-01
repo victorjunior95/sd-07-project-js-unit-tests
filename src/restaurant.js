8d4bd01c158fdbea3322543e784cc9fc8d5b78c8
vitorAmorims <vtamorims@gmail.com>
2020-10-01 18:55:05 -0300

    PASSO 1 criar funçao createMenu() e retornar objeto no formato especificado.

--- src/restaurant.js
+++ src/restaurant.js
@@ -44,7 +44,11 @@
 */
 
 // PASSO 1: Crie uma função `createMenu()` que, dado um objeto passado por parâmetro, retorna um objeto com o seguinte formato: { fetchMenu: objetoPassadoPorParametro }.
-//
+const createMenu = (param) => {
+  let retorno = {};
+  retorno.fetchMenu = param;
+  return retorno;
+}
 // Agora faça o TESTE 2 no arquivo `tests/restaurant.spec.js`.
 
 //------------------------------------------------------------------------------------------
@@ -71,6 +75,6 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+// const createMenu = () => {};
 
 module.exports = createMenu;
