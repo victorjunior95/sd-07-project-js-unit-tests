6fdb0f825158e99861fc77efa5e8cae218393543
Filipi Firmino <contatoic3web@gmail.com>
2020-09-30 21:13:20 -0300

    Requirements 03 CC

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,10 @@ const vqv = (nome, idade) => {
   if (typeof (nome) !== 'string' || typeof (idade) !== 'number') {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!\n`+
-  `Tenho ${idade} anos,\n`+ 
-  `trabalho na Trybe e mando muito em programação!\n`+
-  `#VQV!`;
+  return 'Oi, meu nome é ${nome}! \n'+
+  'Tenho ${idade} anos, \n'+ 
+  'trabalho na Trybe e mando muito em programação! \n'+
+  '#VQV!';
 };
 
 module.exports = vqv;
