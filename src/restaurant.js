5279c7fff71e75b50f8cbeb200dc3642a6956131
Felipe Nascimento <flpnascto@gmail.com>
2020-10-01 17:00:22 -0300

    Desenvolvendo requisito 9: PASSO 03.

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,10 +71,17 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
+
+
+// const orderFromMenu = (request) => {
+//   this.consumption.push(request)
+// }
+
 const createMenu = (objeto) => {
   const menu = {}
   menu.fetchMenu = objeto;
   menu.consumption = [];
+  menu.order = (request) => menu.consumption.push(request);
   return menu
 };
 
