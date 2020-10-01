b6346880d76d2f8de837143bd1934a76f135ace7
vladimirrchagas <vladimirrchagas@gmail.com>
2020-10-01 13:23:16 -0300

    Verificando Requisito 6: productDetails.spec.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,31 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails('Alcool', 'Geladeira'), 'object');
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(productDetails('Alcool', 'Geladeira')).length, 2);
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert(
+      typeof productDetails('Alcool', 'Geladeira')[0] === 'object' 
+      &&
+      typeof productDetails('Alcool', 'Geladeira')[1] === 'object'
+    );
+    
+    
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Alcool', 'Geladeira')[0], productDetails('Alcool', 'Geladeira')[1]);
+
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert(
+      productDetails('Alcool', 'Geladeira')[0].details.productId.substr(productDetails('Alcool', 'Geladeira')[0].details.productId.length - 3) === '123' 
+      && 
+      productDetails('Alcool', 'Geladeira')[1].details.productId.substr(productDetails('Alcool', 'Geladeira')[1].details.productId.length - 3) === '123'
+    );
+    
   });
 });
