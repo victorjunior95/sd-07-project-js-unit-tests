ca5363cf5e4af7b057cc652ab4572c54b1f00f86
Filipi Firmino <contatoic3web@gmail.com>
2020-10-01 16:34:28 -0300

    Requirement 05

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let object = {
+    name: name,
+    feedback: () => {
+      return 'Eita pessoa boa!';
+    },
+  };
+  return object;
+};
 
 module.exports = createStudent;
