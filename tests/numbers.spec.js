b0da081cda72309b3c1d83e22ed37e76da2a8cce
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-10-01 16:09:35 -0300

    Adicionando exercício numbers

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,10 +18,18 @@ const numbers = require('../src/numbers');
 
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    assert.fail();
+    
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    const teste1 = numbers([1, 2, 3, 4, 5]);
+    assert.strictEqual(teste1, true)
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    const teste2 = numbers([1, 2, '3', 4, 5]);
+    assert.strictEqual(teste2, false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    const teste3 = numbers([1, 'a', 3]);
+    assert.strictEqual(teste3, false);
     // Escreva um teste em que a função recebe [' '] e retorna false
+    const teste4 = numbers([' ']);
+    assert.strictEqual(teste4, false);
   });
 });
