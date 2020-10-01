4f892d390ddf85800a3c7c26b474a4e711d48aba
Eduardo Pizorno <pizorno@gmail.com>
2020-10-01 18:09:17 -0300

    productDetails tests done

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,21 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.values(productDetails('Alcool gel', 'Máscara')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const array = productDetails('Alcool gel', 'Máscara');
+    assert.ok(typeof array[0] === 'object' && typeof array[1] === 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    // Teste feito consultando github do Rafael Guimarães
+    // https://github.com/tryber/sd-07-project-js-unit-tests/blob/rafaelmguimaraes-js-unit-tests/tests/productDetails.spec.js
+    const regEx = /123$/;
+    assert.ok(regEx.test(array[0]['details']['productId']) && (regEx.test(array[1]['details']['productId'])));
   });
 });
