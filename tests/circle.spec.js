fa24fc49bb8c9c1065730c4321e2a1d63a7b9ca7
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-09-30 22:57:36 -0300

    circle.spec.js - test if circle output is object

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -24,14 +24,15 @@ const circle = require('../src/circle');
 */
 
 describe('#circle', () => {
-  it('given a radius, should return an object with circles info', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 entradas.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-  });
+    it('given a radius, should return an object with circles info', () => {
+        // ESCREVA SEUS TESTES ABAIXO:
+        // Teste se circle retorna um objeto.
+
+
+        // Teste se o objeto retornado tem 3 entradas.
+        // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+        // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+        // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+        // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    });
 });
