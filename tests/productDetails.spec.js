288443aa200810fbe45b8a168c25575d83de9dab
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-10-01 20:16:00 -0300

    Alteraçoes para o sexto requisito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Mascara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual((productDetails('Alcool gel', 'Mascara').length), 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof Object.keys(productDetails('Alcool gel', 'Mascara')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(Object.entries(productDetails('Alcool gel')), Object.entries(productDetails('Mascara')));
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual((Object.values(productDetails('Alcool gel', 'Mascara')[0]['details']).toString()).endsWith('123'), true);
+    assert.deepStrictEqual((Object.values(productDetails('Alcool gel', 'Mascara')[1]['details']).toString()).endsWith('123'), true);
   });
 });
