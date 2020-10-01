8d1fc4454554e41971d4f9688e31e07ca3246f4f
MoisesSantana <moisaant@gmail.com>
2020-10-01 13:22:13 -0300

    correcoes CC

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,10 +17,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  for (let index = 0; index <= 3; index += 1) {
+    myArray.push(index);
+    for (let index2 = 2; index2 <= 3; index2 += 1) {
+      myArray.push(index2);
     }
   }
   return myArray;
