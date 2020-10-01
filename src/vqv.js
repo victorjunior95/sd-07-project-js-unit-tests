5faf47d5b7cce7cdc65d1e42ccaa313d991892e7
Galaraz <jvictor.si.2013@gmail.com>
2020-10-01 19:17:54 -0300

    xerox gomes the return

--- src/vqv.js
+++ src/vqv.js
@@ -19,10 +19,7 @@
 const vqv = (nome, idade) => {
   if (nome == null && idade == null) {
     return undefined;
-  } return ` Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  } return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
