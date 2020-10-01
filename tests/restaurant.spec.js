f1a457dec1691822291ec79eac9124aa9612a7e4
Fernando Antunes <fernando.antunes1@gmail.com>
2020-10-01 14:12:21 -0300

    Requisito 9 - Primeiro teste e implementação básica da funcão pra atender o teste

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -50,9 +50,9 @@ const createMenu = require('../src/restaurant');
 */
 
 describe('#createMenu', () => {
-  it('tests the function has the correct behaviour', () => {
-    assert.fail();
+  it('tests the function has the correct behaviour', () => {    
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
+    assert.deepStrictEqual(createMenu({ food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} }), { fetchMenu: {food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} } });
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
