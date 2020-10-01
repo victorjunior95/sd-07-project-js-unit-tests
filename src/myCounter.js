7bec3280880ae883e4d31c6054697de382fae9f6
renatomak <renato.mark.silva@gmail.com>
2020-09-30 22:07:08 -0300

    requisito 8 - CC

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,16 +16,14 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let index = 2; index <= 3; index += 1) {
+      myArray.push(index);
     }
   }
   return myArray;
 };
-
-console.log(myCounter());
 
 module.exports = myCounter;
