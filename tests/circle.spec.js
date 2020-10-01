213371b66914008b191ced57c2f7da7bfe1e90c3
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-09-30 21:11:06 -0300

    testando requisito 4 +4x

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,9 +27,9 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.deepStrictEqual(typeof circle(1), 'object');
+    assert.deepStrictEqual(typeof (circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepStrictEqual(Object.entries(circle(1).length), 3);
+    assert.deepStrictEqual(Object.entries(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.deepStrictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
