47ec7d7b65c3caa8b9cc4cef017dc8564a89f057
Mauro Junqueira <maurojunqueira@pop-os.localdomain>
2020-10-01 10:29:20 -0300

    Finalizado requisito 3 eslint2

--- src/vqv.js
+++ src/vqv.js
@@ -23,7 +23,7 @@ const vqv = (nome, idade) => {
   return ' Oi, meu nome é ' + nome + ' !\n ' +
   'Tenho ' + idade + ' anos,\n ' +
   ' trabalho na Trybe e mando muito em programação!\n ' +
-  ' #VQV! '
+  ' #VQV! ';
 };
 
 module.exports = vqv;
