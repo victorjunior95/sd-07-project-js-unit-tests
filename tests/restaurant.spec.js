b389ebc55cbae852d32caa66b8f5fbdf05b755e7
Felipe Nascimento <flpnascto@gmail.com>
2020-10-01 15:46:13 -0300

    Desenvolvendo requisito 9: Ajustando TESTE 01 e 'pass' nos TESTE 01 e TESTE 04.

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -56,7 +56,7 @@ describe('#createMenu', () => {
     // ```
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
-    assert.deepStrictEqual(createMenu({}), { fetchMenu: {}});
+    assert.deepStrictEqual(createMenu({}), { fetchMenu: {}, consumption: [] });
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, verifique que 'objetoRetornado.fetchMenu' retorna um objeto cujas chaves são somente `food` e `drink`.
