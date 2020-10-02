d1025405fe1c2db81c763515a37b11123a98bede
Felipe Nascimento <flpnascto@gmail.com>
2020-10-02 20:27:31 -0300

    Verificando requisito 04.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -36,9 +36,11 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepStrictEqual(circle(2).circumference, 12.56 );
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(3).area, 28.26 );
+    assert.deepStrictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26 );
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 })
+    let actualObject = circle(3);
+    actualObject['area'] = parseFloat(actualObject.area.toPrecision(4));
+    assert.deepStrictEqual(actualObject, { radius: 3, area: 28.26, circumference: 18.84 });
   });
 });
 
