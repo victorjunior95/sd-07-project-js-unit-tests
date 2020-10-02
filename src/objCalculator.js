e2ed057fdb1b74908db645e521fadcb22f1546d1
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-10-02 15:53:51 -0300

    Reuisitos 07 - 02/09/2020

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,42 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const addNumber = (number1, number2) => {
+  const numberInt1 = parseInt(number1);
+  const numberInt2 = parseInt(number2);
+  const sum = numberInt1 + numberInt2;
+  return Math.floor(sum);
+}
 
+const multNumber = (number1, number2) => {
+  const numberInt1 = parseInt(number1);
+  const numberInt2 = parseInt(number2);
+  const mult = numberInt1 * numberInt2;
+  return Math.floor(mult);
+}
+
+const divNumber = (number1, number2) => {
+  const numberInt1 = parseInt(number1);
+  const numberInt2 = parseInt(number2);
+  const div = numberInt1 / numberInt2;
+  return Math.floor(div);
+}
+
+const subNumber = (number1, number2) => {
+  const numberInt1 = parseInt(number1);
+  const numberInt2 = parseInt(number2);
+  const sub = numberInt1 - numberInt2;
+  return Math.floor(sub);
+}
+
+const calculator = {
+  add: addNumber,
+  mult: multNumber,
+  div: divNumber,
+  sub: subNumber
+};
+
+console.log(calculator.add(1, 1)) // Retorno: 2;
+console.log(calculator.div(3, 2)) // Retorno: 1;
+console.log(calculator.sub(1, 3))
 module.exports = calculator;
