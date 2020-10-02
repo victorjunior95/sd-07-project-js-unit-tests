cbd30d3e689e720d47201d5d951ae213ed26f0ac
Marcos R. Silva <marcos.1998226@gmail.com>
2020-10-02 01:07:04 -0300

    refluxo vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,14 +16,11 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {
-  if (nome !== undefined || idade !== undefined) {
-    return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
-  }
-  return undefined;
-};
 
+const vqv = (nome, idade) => {
+  if (typeof nome === 'undefined' || typeof idade === 'undefined') {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+};
 module.exports = vqv;
