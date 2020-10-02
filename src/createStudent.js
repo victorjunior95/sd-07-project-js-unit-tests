d5580617e041782a77c00e8189dd82fb79f4c288
berchon <berchon@gmail.com>
2020-10-01 23:00:27 -0300

    Feito do item 1 até o 8

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (firstname) => {
+  const student = {
+    name: firstname,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return (student);
+};
 
 module.exports = createStudent;
