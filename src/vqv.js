19ac1298f05479058cc570d1cee1731a3b58f337
Demistocles <demi.treze@gmail.com>
2020-10-02 19:16:45 -0300

    enviando o terceiro requisito

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  const phrase = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mndo muito em programação!
+  #VQV!`;
+  return phrase;
+}
 
 module.exports = vqv;
