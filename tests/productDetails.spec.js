de396ca7ced26d0ca3bbb45fe71fb1ef0246da99
spawnred <bruno.spawnhell@gmail.com>
2020-10-01 21:42:05 -0300

    item 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,8 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // const product = productDetails();
     // ESCREVA SEUS TESTES ABAIXO:
+    assert.strictEqual(Array.isArray(productDetails()), true);
+    assert.strictEqual(Object.keys(productDetails()).length, 2);
+    assert.strictEqual(typeof productDetails(), 'object');
+    assert.notStrictEqual(productDetails()[0], productDetails()[1]);
+    assert.deepStrictEqual(
+      productDetails()[0].details.productId.endsWith('123') &&
+      productDetails()[1].details.productId.endsWith('123'),
+      true,
+    );
     // Teste que o retorno da função é um array.
     // Teste que o array retornado pela função contém dois itens dentro.
     // Teste que os dois itens dentro do array retornado pela função são objetos.
