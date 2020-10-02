dae33f3e880463485031f96dcf7243982690700e
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-02 09:35:46 -0300

    6) implementação de testes para a função productDetails.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,25 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(typeof productDetails('chocolate', 'cerveja'), 'object');
+    assert.strictEqual(productDetails('chocolate', 'cerveja').length, 2);
+    assert.strictEqual(
+      typeof productDetails('chocolate', 'cerveja')[0] &&
+        typeof productDetails('chocolate', 'cerveja')[1],
+      'object'
+    );
+    assert.notStrictEqual(
+      productDetails('chocolate', 'cerveja')[0],
+      productDetails('chocolate', 'cerveja')[1]
+    );
+    assert.deepStrictEqual(
+      productDetails('chocolate', 'cerveja')[0].details.productId.endsWith(
+        '123'
+      ) &&
+        productDetails('chocolate', 'cerveja')[1].details.productId.endsWith(
+          '123'
+        ),
+      true
+    );
   });
 });
