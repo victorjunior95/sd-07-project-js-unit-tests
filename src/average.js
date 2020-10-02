aa1568220c831d0f9fe10584fd2bdcadc23f4a25
Galaraz <jvictor.si.2013@gmail.com>
2020-10-01 23:05:49 -0300

    the return agora sim

--- src/average.js
+++ src/average.js
@@ -12,8 +12,7 @@
     - average([1, '2']) // Retorno: undefined;
 */
 const average = (array) => {
-  
-  if (array.length === 0) {
+   if (array.length === 0) {
     return undefined;
   }
   let element = 0;
