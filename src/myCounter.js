711656e133155f30616084619de8984d5b60ae65
Strongreen <strongreen@live.com>
2020-10-01 22:39:33 -0300

    exercicio 8 - correçao CC

--- src/myCounter.js
+++ src/myCounter.js
@@ -28,12 +28,10 @@ const myCounter = () => {
   }
   for (var counter = 2; counter < 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter < 3; counter += 1) {
+    myArray.push(counter);
+    for (var counter = 3; counter < 4; counter += 1) {
       myArray.push(counter);
-      if (counter === 2) {
-        myArray.push(counter+1);
-        myArray.push(counter+1);
-      }
+      myArray.push(counter);
     }
   }
   for (var counter = 2; counter <= 3; counter += 1) {
