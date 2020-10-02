2bb0c9cd88f3bbfc59f90a45ca898d1c823f4c10
Anderson Santana <anderslash@gmail.com>
2020-10-02 13:56:39 -0300

    Requisito 6 concluido

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,7 +36,7 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    assert.strictEqual(Array.isArray(productDetails('Celular', 'Controle')), true);  // Teste que o retorno da função é um array. (Fonte: https://stackoverflow.com/questions/4775722/how-to-check-if-an-object-is-an-array)
+    assert.strictEqual(Array.isArray(productDetails('Celular', 'Controle')), true);  // Teste que o retorno da função é um array. (Fonte: Oliva deu a dica no Slack, https://stackoverflow.com/questions/4775722/how-to-check-if-an-object-is-an-array)
     assert.strictEqual(productDetails('Celular', 'Controle').length, 2);             // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(typeof productDetails('Celular', 'Controle')[0,1], 'object')  // Teste que os dois itens dentro do array retornado pela função são objetos.   
 
@@ -49,6 +49,16 @@ describe('#productDetails', () => {
     }
     assert.strictEqual(comparaObjetos(), true); // Teste que os dois objetos são diferentes entre si.
 
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    const verifyProductIdEnding = () => {
+      if (productDetails('Celular', 'Controle')[0].details.productId.substr(-3) != 123) { // Colega Vladimir Chagas deu a dica do substr no Slack https://trybecourse.slack.com/archives/C017W4EDD4K/p1601576748042600
+        return false
+      };
+      if (productDetails('Celular', 'Controle')[1].details.productId.substr(-3) != 123) {
+        return false
+      };
+      return true;
+    }
+
+    assert.strictEqual(verifyProductIdEnding(), true);  // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
