3dafc469eeed7d3eaa772317978e87e6a82c1388
Marcos R. Silva <marcos.1998226@gmail.com>
2020-10-02 17:11:36 -0300

    createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const pessoa = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return pessoa;
+};
 
 module.exports = createStudent;
