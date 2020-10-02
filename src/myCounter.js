a1f49d8603ef310bbf48c591436b66f47f44b9c3
Bruna Fonsêca <1998bfonseca@gmail.com>
2020-10-02 15:25:04 -0300

    requisition 8 fixe

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,10 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
+    for (counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
