c65e7ca97e4edc2e7d236c6e78ac28ac56fa519b
Brune Miguel <bstfreire@gmail.com>
2020-10-03 13:19:52 -0300

    Atualizando commit

--- src/average.js
+++ src/average.js
@@ -13,10 +13,10 @@
 */
 
 const average = (array) => {
-  if(array === 0 || typeof array !== 'number') { return undefined; }
+  if (array === 0 || typeof array !== 'number') { return undefined; }
   let sum = 0;
-  for(let index = 0; index < array.length; index += 1){
-    sum += array[index]
+  for (let index = 0; index < array.length; index += 1) {
+    sum += array[index];
   }
   return Math.round(sum / array.length);
 };
