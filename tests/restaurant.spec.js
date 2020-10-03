e0996f9fcec7ac306fb55f826961d569a2eb9f9f
Rita Jeveaux <ritajeveaux@gmail.com>
2020-10-02 22:32:43 -0300

    Requisito_9

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -6,7 +6,7 @@ const createMenu = require('../src/restaurant');
 
 /*
   Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
-  - ler o menu cadastrado;
+ + - ler o menu cadastrado;
   - fazer pedidos;
   - verificar o que foi pedido;
   - somar o valor da conta.
@@ -51,9 +51,8 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
-    // ```
+    assert.deepStrictEqual(createMenu(objetoQualquer), {fetchMenu: objetoQualquer,});
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
     // ```
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
