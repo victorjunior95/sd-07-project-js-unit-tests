cc52cb572c208e4d9b1629684eedc01a47098112
Brune Miguel <bstfreire@gmail.com>
2020-10-03 15:26:46 -0300

    Arrumando requisito 2

--- src/average.js
+++ src/average.js
@@ -13,7 +13,7 @@
 */
 
 const average = (array) => {
-  if (array === 0 || typeof array !== 'number') { return undefined; }
+  if (array === 0 typeof array !== 'number') { return undefined; }
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
     sum += array[index];
