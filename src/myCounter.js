20d7837f1dabad191756aca8dec2db2a6f01214e
Lorena Góes <goesloren@gmail.com>
2020-10-02 22:26:04 -0300

    Requisito 8, corrigindo codeclimate

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter < 4; counter += 1) {
     myArray.push(counter);
     for (let otherCounter = 2; otherCounter < 4; otherCounter += 1) {
