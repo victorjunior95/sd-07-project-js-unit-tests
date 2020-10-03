1586c7a088f690c6e52816841def2bba7b97b0e9
Brune Miguel <bstfreire@gmail.com>
2020-10-03 17:02:15 -0300

    Corrigindo cc do requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if ((typeof (nome) !== 'string') || (typeof (idade) !== 'number')) { 
+  if ((typeof (nome) !== 'string') || (typeof (idade) !== 'number')) {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
-};
+};  
 
 module.exports = vqv;
