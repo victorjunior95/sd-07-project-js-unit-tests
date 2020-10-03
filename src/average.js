6efe0a7442937c759a8fc38d570ffee0eddb2b1f
Brune Miguel <bstfreire@gmail.com>
2020-10-03 14:55:15 -0300

    Atualizando requisito 1

--- src/average.js
+++ src/average.js
@@ -18,7 +18,7 @@ const average = (array) => {
   for (let index = 0; index < array.length; index += 1) {
     sum += array[index];
   }
-  return Math.round(sum / array.length);
+  return Math.round(sum / (array.length));
 };
 
 module.exports = average;
