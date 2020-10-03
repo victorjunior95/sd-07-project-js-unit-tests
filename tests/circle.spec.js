1fdf4ca5cca0a355182dd681cb2c59c5fa431280
Jr3564 <rodolfojr94@gmail.com>
2020-10-03 19:57:47 -0300

    Requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -13,7 +13,7 @@ const circle = require('../src/circle');
     - Um número inteiro. Exemplos: 1; 3;
   Comportamento:
     - circle(1) // Retorno: {radius: 1, area: 3.14, circumference: 6.28}
-    - circle(7) // Retorno: {radius: 7, area: 153.86, circumference: 43.96}
+    - circle(7) // Retorno: radius: 7, area: 153.86, circumference: 43.96
     - circle(3) // Retorno: {radius: 3, area: 28,26, circumference: 18.84}
 
   DICA: Números de ponto flutuante em JavaScript são imprecisos!  Para testar, vá no seu navegador e faça `0.2 + 0.1`.
@@ -25,13 +25,18 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(circle(2).circumference, 12.56);
+    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3
+    assert.strictEqual(circle(3).area, 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
   });
 });
