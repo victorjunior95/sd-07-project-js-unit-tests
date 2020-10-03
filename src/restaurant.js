8561ae99de47e6fe3ed84fe2155580ac124ddb91
Alstent <rafaelasltent@gmail.com>
2020-10-03 19:08:35 +0200

    resolvendo code climate

--- src/restaurant.js
+++ src/restaurant.js
@@ -72,10 +72,12 @@
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
 const createMenu = (menu) => {
-  const res = { 
+  const res = {
     fetchMenu: menu,
     consumption: [],
-    order: (pedido) => res.consumption.push(pedido),
+    order: (pedido) => {
+      res.consumption.push(pedido)
+    },
     pay: () => {
       let value = 0;
       res.consumption.forEach((pedido) => {
@@ -86,9 +88,9 @@ const createMenu = (menu) => {
         });
       });
       return Math.round(value);
-    },
-  }
-  return res
+    }
+  };
+  return res;
 };
 
 module.exports = createMenu;
