30c6d1b51dac4585086efb9055a065780133bb4b
Brune Miguel <bstfreire@gmail.com>
2020-10-03 16:07:54 -0300

    Corrigindo o CC

--- src/average.js
+++ src/average.js
@@ -14,16 +14,17 @@
 
 const average = (array) => {
   if (array.length === 0) {
-    return undefined; 
+    return undefined;
   }
 
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[i] !== 'number') { 
-      return undefined; }
+    if (typeof array[i] !== 'number')
+      return undefined;
     sum += array[index];
-  }
-  return Math.round(sum / array.length);
+  };
+  const result =  Math.round(sum / array.length);
+  return result;
 };
 
 module.exports = average;
