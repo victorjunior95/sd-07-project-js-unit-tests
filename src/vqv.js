2b670cbfe19ebc892aa5ee6dcc92b7de5a7d17ee
Breno Alves <brenoalv3ss@gmail.com>
2020-10-03 16:23:45 -0300

    COrreções

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,8 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === undefined && idade === undefined) {
-    return (undefined);
-  }
-  return (`Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`);
+  if (nome === undefined || idade === undefined) return undefined;
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
