fbece9099ac432206ef3e8494038957a59f1d83a
Brune Miguel <bstfreire@gmail.com>
2020-10-03 16:28:20 -0300

    Corrigindo requisito 1

--- src/average.js
+++ src/average.js
@@ -19,7 +19,7 @@ const average = (array) => {
 
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[i] !== 'number') {
+    if (typeof array[index] === 'string') {
       return undefined;
     }
     sum += array[index];
