160950c3c7cf3aacdf604be3db81a97162e40f6e
Breno Alves <brenoalv3ss@gmail.com>
2020-10-03 15:44:35 -0300

    Correção vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -20,9 +20,9 @@ const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
     return (undefined);
   }
-  return (`Oi, meu nome é ${nome}! \n
-  Tenho ${idade} anos, \n
-  trabalho na Trybe e mando muito em programação! \n
+  return (`Oi, meu nome é ${nome}!\n
+  Tenho ${idade} anos,\n
+  trabalho na Trybe e mando muito em programação!\n
   #VQV!`);
 };
 
