0817c1541f6efab9e76aaea8c38e48db6fc23b50
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-10-03 20:09:34 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let count = 2; count <= 3; count += 1) {
