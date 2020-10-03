37eb76a92e43a4ba55926229fe68a069e771236a
Breno Alves <brenoalv3ss@gmail.com>
2020-10-03 15:39:19 -0300

    Correção vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -18,13 +18,12 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
-    return undefined;
+    return (undefined);
   }
-  const paragrafo = `Oi, meu nome é ${nome}! \n`
-  + `Tenho ${idade} anos, \n`
-  + 'trabalho na Trybe e mando muito em programação! \n'
-  + '#VQV!';
-  return paragrafo;
+  return (`Oi, meu nome é ${nome}! \n
+  Tenho ${idade} anos, \n
+  trabalho na Trybe e mando muito em programação! \n
+  #VQV!`);
 };
 
 module.exports = vqv;
