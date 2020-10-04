83e4b5be6a3914ec671bfc22e5c2c5222794a2a1
Lucas Sá <lucas.nonato1@hotmail.com>
2020-10-03 21:07:19 -0300

    Tentativa 3 função vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -20,11 +20,12 @@ const vqv = (nome, idade) => {
   if (nome === '' && idade === '') {
     return undefined;
   }
-  return `
-    Oi, meu nome é ${nome}!
-    tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+  return (
+    `Oi, meu nome é ${nome}!\n` +
+    `Tenho ${idade} anos,\n` +
+    'trabalho na Trybe e mando muito em programação!\n' +
+    '#VQV!'
+  );
 };
 
 module.exports = vqv;
