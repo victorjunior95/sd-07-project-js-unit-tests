a83e3589674707a1681b3e03b2336cd9f7b74a97
Lucas Sá <lucas.nonato1@hotmail.com>
2020-10-04 03:08:18 -0300

    Criação teste product details 3/5

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,11 +33,20 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(
+      productDetails('Alcool gel', 'Máscara') instanceof Array,
+      true
+    );
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(
+      productDetails('Alcool gel', 'Máscara')[0] instanceof Object &&
+        productDetails('Alcool gel', 'Máscara')[1] instanceof Object,
+      true
+    );
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
