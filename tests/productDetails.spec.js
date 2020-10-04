88d1350057c0902dffe3e66a36c1f80baf6902e0
Jr3564 <rodolfojr94@gmail.com>
2020-10-04 17:26:21 -0300

    requisito 4

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,16 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    for( let num of productDetails('Alcool gel', 'Máscara'))
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois objetos são diferentes entre si.
+
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
