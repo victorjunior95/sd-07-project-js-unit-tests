a598d4a298a13aa5f8b80b4603b068a44428852b
Geziel <geziel.fernandez@pucp.pe>
2020-10-04 15:18:52 -0300

    third file again again

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,7 @@ const vqv = (nome, idade) => {
   if (typeof nome === 'undefined' || typeof idade === 'undefined') {
     return undefined;
   }
-  return `'Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
