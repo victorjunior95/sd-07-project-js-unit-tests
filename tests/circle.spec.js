dcc981b20976c2e4fc7a580d605214c1793f590c
Lets <letsferreiratx@gmail.com>
2020-10-04 20:26:01 -0300

    createStudent funcao ok

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -39,5 +39,6 @@ describe('#circle', () => {
     assert.strictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 })
+    //Foi a soluçao q achei mais correta, porem minha opiniao é que o erro de precisao esta dentro da funçao e nao no teste.
   });
 });
