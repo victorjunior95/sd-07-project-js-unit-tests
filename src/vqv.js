df989ad25542010c14b1a7a3e3db854365c10bf2
Geziel <geziel.fernandez@pucp.pe>
2020-10-04 15:10:21 -0300

    adjusting code climate

--- src/vqv.js
+++ src/vqv.js
@@ -16,11 +16,11 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {;
-  if(typeof nome === 'undefined' || typeof idade === 'undefined') {
-    return undefined
+const vqv = (nome, idade) => {
+  if (typeof nome === 'undefined' || typeof idade === 'undefined') {
+    return undefined;
   }
-  return `'Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV!`
-}
+  return `'Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV!`;
+};
 
 module.exports = vqv;
