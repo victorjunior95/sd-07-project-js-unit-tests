c3ddd7aea22d08c7d02bd731793d015a62749f19
richardfazeredo <richardcompletetour@gmail.com>
2020-10-04 00:35:19 -0300

    Requirement 2 - implementing test cases in the function numbers - final correction

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -4,8 +4,8 @@ const numbers = require('../src/numbers');
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
     assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
-    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), true);
-    assert.deepStrictEqual(numbers([1, 'a', 3]), true);
-    assert.deepStrictEqual(numbers(' '), true);
+    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
+    assert.deepStrictEqual(numbers([1, 'a', 3]), false);
+    assert.deepStrictEqual(numbers(' '), false);
   });
 });
