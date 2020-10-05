1e0adabcf8e8b705e04d7c611d128e2d848216bc
Iury Nathan <iurynathan@gmail.com>
2020-10-05 13:54:07 -0300

    requisito circle

--- src/circle.js
+++ src/circle.js
@@ -4,7 +4,7 @@
   Essa função recebe o raio de um círculo,
   retornando um objeto contendo suas informações (Raio, Área e Circunferência).
   Se não for especificado um raio, a função retorna undefined.
-  Elabore testes para verificar se a função está correta.
+  +função está correta.
 
   Parâmetros:
     - Um número inteiro. Exemplos: 1; 3;
