e74d7a046cd4a9f6e7127b497694077243423568
Brune Miguel <bstfreire@gmail.com>
2020-10-05 20:22:43 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const student = {};
 
-module.exports = createStudent;
+  student.name = name;
+  student.feedback = () => 'Eita pessoa boa!';
+  return student; 
+};
+
+
+module.exports = createStudent; 
