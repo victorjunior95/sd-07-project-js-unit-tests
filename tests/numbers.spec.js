494418a2ff080746049246f0ef3598dbbf32a438
Leonardo <lecastro16@gmail.com>
2020-10-05 14:06:57 -0300

    exercicio 1 ao 4 v1

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -23,8 +23,8 @@ describe('#numbers', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     assert.deepStrictEqual([1, 2, '3', 4, 5], 'false');
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    assert.deepStrictEqual([1, "a", 3], 'false');
+    assert.deepStrictEqual([1, 'a', 3], 'false');
     // Escreva um teste em que a função recebe [' '] e retorna false
-    assert.deepStrictEqual([" "], 'false');
+    assert.deepStrictEqual([' '], 'false');
   });
 });
