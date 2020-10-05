1b5691b812ff72ec34cc7b51634aa30d5d7d4607
alexandrefaustino <alexandrefauldade@gmail.com>
2020-10-05 08:42:57 -0300

    Commit 0.2.7

--- src/circle.js
+++ src/circle.js
@@ -17,15 +17,21 @@
         Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
 */
-
+const assert = require('assert');
 const circle = (radius) => {
   const PI = 3.14;
   if (!radius) { return undefined; }
   return {
     radius,
-    area: PI * radius * radius,
+    area: parseFloat((PI * radius * radius).toFixed(2)),
     circumference: 2 * PI * radius,
   };
 };
+
+assert.deepStrictEqual(circle(1), {radius: 1, area: 3.14, circumference: 6.28});
+
+assert.deepStrictEqual(circle(7), {radius: 7, area: 153.86, circumference: 43.96});
+
+assert.deepStrictEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
 
 module.exports = circle;
