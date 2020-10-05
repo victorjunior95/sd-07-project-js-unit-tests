892f1ba83c9e26b2452b5c2eb61b95cdb640326c
Camila Valentim <camilaifpb@hotmail.com>
2020-10-04 21:21:51 -0300

    funcao average

--- src/average.js
+++ src/average.js
@@ -13,14 +13,10 @@
 */
 
 const average = (array) => {
-  if (array === 0) {
-    return undefined;
-  }
+  if (array.length === 0) return undefined;
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] === 'string') {
-      return undefined;
-    }
+    if (typeof array[index] !== 'number') return undefined;
     sum += array[index];
   }
   const mediaArray = Math.round(sum / array.length);
