42a8b544cb05961a1086317eba169078dfa9ece0
Geziel <geziel.fernandez@pucp.pe>
2020-10-05 10:16:29 -0300

    sisth file

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,9 +36,15 @@ describe('#productDetails', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails(), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails()[0] && typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.ok(productDetails()[0] !== productDetails()[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.ok(productDetails()[0].details.productId.slice(-3) === '123');
+    assert.ok(productDetails()[1].details.productId.slice(-3) === '123');
   });
 });
