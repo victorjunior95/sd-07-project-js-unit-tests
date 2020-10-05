f0a0796c274fe55ce017479bf764e81dfda51a5c
Emerson Nogueira Junqueira <emoonlit@pop-os.localdomain>
2020-10-04 21:10:49 -0300

    Add floor in objCalculator and solution myCounter, fix let index

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let index = 2; index <= 3; index += 1) {
+      myArray.push(index);
     }
   }
   return myArray;
