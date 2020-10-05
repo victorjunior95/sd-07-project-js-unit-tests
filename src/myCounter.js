197eb7f1deaaa62c6958ea89161245f71a514846
alexandrefaustino <alexandrefauldade@gmail.com>
2020-10-05 03:08:00 -0300

    Commit 0.2.4

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,10 +16,11 @@
 */
 
 const myCounter = () => {
+  let counter = 0;
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
