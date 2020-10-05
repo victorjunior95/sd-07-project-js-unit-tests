3878ba22fcb8b9afe63a67d1a0b8a24d22401701
Camila Valentim <camilaifpb@hotmail.com>
2020-10-04 22:22:11 -0300

    requisito vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  }
+  const paragraph = `Oi, meu nome é ${nome}!\n`
+  + `Tenho ${idade} anos, \n` +
+  'trabalho na Trybe e mando muito em programação!\n'
+  + '#VQV!';
+  return paragraph; 
+};
 
 module.exports = vqv;
