b80bf5363ab1ee15fc8442c49b83587f61da82c4
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-10-05 16:31:48 -0300

    solucionando git problem

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof (arr[i]) !== 'number') {
+      return undefined;
+    }
+    sum += arr[i];
+  }
+  const avrg = Math.round(sum / arr.length);
+  return avrg;
+};
 
 module.exports = average;
