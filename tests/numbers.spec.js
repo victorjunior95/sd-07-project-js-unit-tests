f02f677e86576b973ac68fa20301656e13fb2620
virgilio-ferreira <virgilio.ferreiraneto@gmail.com>
2020-10-05 20:01:50 -0300

    Segundo requisito_3

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -19,12 +19,12 @@ const numbers = require('../src/numbers');
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    assert.strictEqual(average([1, 2, 3, 4, 5]), true);
+    assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    assert.strictEqual(average([1, 2, '3', 4, 5]), false);
+    assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    assert.strictEqual(average([1, 'a', 3]), false);
+    assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
-    assert.strictEqual(average([' ']), false);
+    assert.strictEqual(numbers([' ']), false);
   });
 });
