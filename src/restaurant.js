2a5f2d447fc0d54781e294a19408a2c596de4710
Vanessa Oliveira <vanessanaara@gmail.com>
2020-10-05 18:59:22 -0300

    Corrigindo arquivo restaurant.js para o codeclimate

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,8 +71,8 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = (object) => ({
-  fetchMenu: object, 
+const createMenu = object => ({
+  fetchMenu: object,
   consumption: [],
 });
 
