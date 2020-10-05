d0671e1d01ff76f7ffa7c48fc72cbb16aae7d13c
Luise Rios <luisemiranda@gmail.com>
2020-10-05 12:44:06 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -12,7 +12,7 @@ const productDetails = require('../src/productDetails');
   - Uma string;
 
   Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
+  productDetails('Alcool gel', 'b') // Retorna:
   [
     {
       name: 'Alcool gel'
@@ -21,7 +21,7 @@ const productDetails = require('../src/productDetails');
       }
     },
     {
-      name: 'Máscara'
+      name: 'b'
       details: {
         productId: 'Máscara123'
       }
@@ -33,12 +33,33 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
+       // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    let received = Array.isArray(productDetails('a', 'b'));
+    let expected = true;
+    assert.strictEqual(received, expected);
+    
     // Teste que o array retornado pela função contém dois itens dentro.
+    received = Object.keys(productDetails('a', 'b')).length;
+    expected = 2;
+    assert.strictEqual(received, expected);
+    
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    array = productDetails('a', 'b');
+    received = array.every(element => !Array.isArray(element) && typeof element === 'object' && element !== null);
+    expected = true;
+    assert.strictEqual(received, expected);
+
     // Teste que os dois objetos são diferentes entre si.
+    array = productDetails('a', 'b');
+    received = array[0];
+    expected = array[1];
+    assert.notDeepStrictEqual(received, expected);
+
     // (Difícil) Teste que os dois productIds terminam com 123.
+    array = productDetails('a', 'b');
+    received = array[0].details.productId.slice(-3) === '123';
+    expected = true;
+    assert.strictEqual(received, expected);
   });
 });
