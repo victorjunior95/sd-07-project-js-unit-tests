be0daddbf26538be2977e2a0810232e829b08de9
Iury Nathan <iurynathan@gmail.com>
2020-10-05 12:15:23 -0300

    corrigindo erro

--- src/numbers.js
+++ src/numbers.js
@@ -8,7 +8,7 @@
     - numbers([2, 3, 4]); // Retorna: true
     - numbers([2, 'errado', 5]); // Retorna: false
 */
-
+const assert = require('assert');
 const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
     if (typeof myArray[i] !== 'number') {
@@ -17,5 +17,8 @@ const numbers = (myArray) => {
   }
   return true;
 };
-
+assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
+assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
+assert.strictEqual(numbers([1, 'a', 3]), false);
+assert.strictEqual(numbers([' ']), false);
 module.exports = numbers;
