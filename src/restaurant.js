77eff8e720c1fd73c90dce8265ab28d928176c16
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-10-06 12:30:21 -0300

    test ISSUES

--- src/restaurant.js
+++ src/restaurant.js
@@ -28,15 +28,24 @@ const createMenu = (obj) => {
 
 
 
-//toda função pay foi inspirada e adaptada a partir duma discussão do slack, que surgiu de Jéssica de Paula,
-//mas houveram  modificações,afim de adaptar para o formato de criação de objeto, e da função definida,
-//além de  correções para verificar quaisquer valores (Uso de toFixed() e Multiply by one)
+// toda função pay foi inspirada e adaptada a partir duma 
+// discussão do slack, que surgiu de Jéssica de Paula,
+// mas houveram  modificações,afim de adaptar para o formato
+// de criação de objeto, e da função definida,
+// além de  correções para verificar quaisquer 
+//valores (Uso de toFixed() e Multiply by one)
 
-//referência para toFixed():https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-//referência para multiply by one: https://stackabuse.com/javascript-convert-string-to-number/
-//referência para Object.assign(): https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-//referência para Object.keys(): https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
-//referência para Object.values():https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/values
-//referência  para o trabalho de Paula: https://github.com/tryber/sd-07-project-js-unit-tests/pull/31
+//referência para toFixed():
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+// referência para multiply by one: 
+// https://stackabuse.com/javascript-convert-string-to-number/
+// referência para Object.assign():
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+// referência para Object.keys(): 
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+//referência para Object.values():
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+// referência  para o trabalho de Paula:
+// https://github.com/tryber/sd-07-project-js-unit-tests/pull/31
 
 module.exports = createMenu;
