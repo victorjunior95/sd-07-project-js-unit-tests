c953cc51de0fa53218d4fff5842a80ae31dd68e5
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-10-06 01:20:44 -0300

    Requisito 1 alterado conforme solicitaçoes do codeclimate

--- src/average.js
+++ src/average.js
@@ -21,7 +21,7 @@ const average = (array) => {
     soma += array[i];
   }
   const resp = soma / array.length;
-  return math.round(resp); /* pesquisado em: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round */
+  return math.round(resp);
 };
 
 module.exports = average;
