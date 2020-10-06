e2d5f92805cb75152bda2c1857d1bb49c6aa3221
Iury Nathan <iurynathan@gmail.com>
2020-10-06 16:08:12 -0300

    corrigindo requisito VQV

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,6 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-const assert = require("assert")
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
@@ -26,31 +25,5 @@ Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`);
 };
-
-assert.strictEqual(typeof vqv, 'function');
-assert.strictEqual(typeof vqv('Tunico', 30), 'string');
-assert.strictEqual(
-  vqv('Tunico', 29),
-  'Oi, meu nome é Tunico!\n' +
-  'Tenho 29 anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!',
-);
-assert.strictEqual(
-  vqv('Alberto', 30),
-  'Oi, meu nome é Alberto!\n' +
-  'Tenho 30 anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!',
-);
-assert.strictEqual(
-  vqv('Hamaji', 29),
-  'Oi, meu nome é Hamaji!\n' +
-  'Tenho 29 anos,\n' +
-  'trabalho na Trybe e mando muito em programação!\n' +
-  '#VQV!',
-);
-assert.strictEqual(vqv(), undefined);
-
 
 module.exports = vqv;
