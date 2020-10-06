8f315e2ecfa592849543c9b66f05efdc3d6a3ae6
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-10-06 17:39:18 -0300

    finalizando a terceira

--- src/createStudent.js
+++ src/createStudent.js
@@ -4,8 +4,6 @@
     (1) name, contendo o nome passado como parâmetro;
     (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
 
-  Faça a função da chave feedback com arrow functions!
-
   Parâmetros:
     - Uma string;
   Comportamento:
@@ -15,6 +13,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameAttr) => {
+  if (typeof nameAttr !== 'string') {
+    return undefined;
+  }
+  const object = {
+    name: nameAttr,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return object;
+};
+
 
 module.exports = createStudent;
