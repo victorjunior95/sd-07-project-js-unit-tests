e657a1d2c6986060b35c18b03e2cb338ad4f0648
paulohp7 <ph.pereira.7@gmail.com>
2020-10-06 11:17:06 -0300

    Third objective with fixes4

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,10 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) return undefined;
-  return `Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV!`;
+  return `Oi, meu nome é ${name}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
 };
 
 module.exports = vqv;
