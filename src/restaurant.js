ffa42330f95a26ca2bfce49c35062b697969b8cf
PedroMarqDev <PedroMarqDev@gmail.com>
2020-10-06 11:15:17 -0300

    requisito 7

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,4 +1,4 @@
-/* eslint-disable max-len */
+l/* eslint-disable max-len */
 
 /*
   Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require("assert");
-const circle = require("../src/circle");
+const assert = require('assert');
+const circle = require('../src/circle');
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -23,21 +23,14 @@ const circle = require("../src/circle");
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe("#circle", () => {
-  it("given a radius, should return an object with circles info", () => {
+describe('#circle', () => {
+  it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    assert.strictEqual(typeof circle(1), "object");
-    assert.strictEqual(Object.keys(circle(1)).length, 3);
-    assert.strictEqual(circle(), undefined);
-    assert.strictEqual(Object.values(circle(2))[2], 12.56);
-    assert.strictEqual(
-      parseFloat(Object.values(circle(3))[1]).toPrecision(2),
-      "28"
-    );
-    assert.deepStrictEqual(Object.values(circle(3)), [
-      3,
-      28.259999999999998,
-      18.84,
-    ]);
+    assert.strictEqual(typeof(circle(1)), 'object')
+    assert.strictEqual(Object.keys(circle(1)).length, 3)
+    assert.strictEqual((circle()), undefined)
+    assert.strictEqual(Object.values(circle(2))[2], 12.56)
+    assert.strictEqual(parseFloat((Object.values(circle(3))[1])).toPrecision(2), '28')
+    assert.deepStrictEqual(Object.values(circle(3)), [3, 28.259999999999998, 18.84])
+    })
   });
-});
