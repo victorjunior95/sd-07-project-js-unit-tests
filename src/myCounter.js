9191c49496aa9b4680b85c558efe7c36deda0ec0
wellington <profwellington17@gmail.com>
2020-10-05 23:13:51 -0300

    fix3 atividade 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,9 +19,9 @@
    for (let counter = 0; counter <= 3; counter += 1) {
      myArray.push(counter);
      for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-     myArray.push(counter2);
-      }
+       myArray.push(counter2);
+     }
    }
    return myArray;
  };
-module.exports = myCounter;
+ module.exports = myCounter;
