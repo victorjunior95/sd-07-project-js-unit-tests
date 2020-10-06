81d4246a2ac303c3f1f742b42677383d86f7ad36
Camila Valentim <camilaifpb@hotmail.com>
2020-10-06 12:11:04 -0300

    code climate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,7 +20,7 @@
 */
 
 const calculator = {
-  add: (number1 , number2) => number1 + number2,
+  add: (number1, number2) => number1 + number2,
   mult: (number1, number2) => number1 * number2,
   div: (number1, number2) => Math.round(Math.floor(number1 / number2)),
   sub: (number1, number2) => number1 - number2,
