86f80f3daaf2e5d908fe8c3824d35aab85638d7e
Camila Valentim <camilaifpb@hotmail.com>
2020-10-06 12:08:13 -0300

    codec limate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,8 +20,8 @@
 */
 
 const calculator = {
-  add : (number1 , number2) => number1 + number2, 
-  mult:(number1, number2) => number1 * number2,
+  add: (number1 , number2) => number1 + number2,
+  mult: (number1, number2) => number1 * number2,
   div: (number1, number2) => Math.round(Math.floor(number1 / number2)),
   sub: (number1, number2) => number1 - number2,
 };
