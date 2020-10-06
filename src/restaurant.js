cac4d2ecf0f01c04d1a7ef20f5987c6b65889733
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-10-06 12:38:51 -0300

    test ISSUES

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,47 +1,46 @@
 let sum = 0;
 const objRetorno = {};
 const inputConsumption = (str) => {
-    objRetorno.consumption.push(str);
+  objRetorno.consumption.push(str);
 };
 const payFunction = (item) => {
-    const food = (objRetorno.fetchMenu.food);
-    const drink = (objRetorno.fetchMenu.drink);
-    const total = Object.assign({}, food, drink);
-    for (let i = 0; i < (Object.keys(total).length); i += 1) {
-        if (item === Object.keys(total)[i]) {
-            sum += Object.values(total)[i];
-        }
-    }
-    return sum;
+  const food = (objRetorno.fetchMenu.food);
+  const drink = (objRetorno.fetchMenu.drink);
+  const total = Object.assign({}, food, drink);
+  for (let i = 0; i < (Object.keys(total).length); i += 1) {
+    if (item === Object.keys(total)[i]) {
+      sum += Object.values(total)[i];
+      }
+  }
+  return sum;
 };
 const createMenu = (obj) => {
-    objRetorno.fetchMenu = obj;
-    objRetorno.consumption = [];
-    objRetorno.order = (inputConsumption);
-    objRetorno.pay = () => {
-        const orderConsumption = objRetorno.consumption;
-        orderConsumption.forEach(payFunction);
-        return ((sum * 1.1).toFixed(2)) * 1;
-    };
-    return objRetorno;
+  objRetorno.fetchMenu = obj;
+  objRetorno.consumption = [];
+  objRetorno.order = (inputConsumption);
+  objRetorno.pay = () => {
+    const orderConsumption = objRetorno.consumption;
+      orderConsumption.forEach(payFunction);
+      return ((sum * 1.1).toFixed(2)) * 1;
+  };
+  return objRetorno;
 };
 
 
-
-// toda função pay foi inspirada e adaptada a partir duma 
+// toda função pay foi inspirada e adaptada a partir duma
 // discussão do slack, que surgiu de Jéssica de Paula,
 // mas houveram  modificações,afim de adaptar para o formato
 // de criação de objeto, e da função definida,
-// além de  correções para verificar quaisquer 
+// além de  correções para verificar quaisquer
 //valores (Uso de toFixed() e Multiply by one)
 
 //referência para toFixed():
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
-// referência para multiply by one: 
+// referência para multiply by one:
 // https://stackabuse.com/javascript-convert-string-to-number/
 // referência para Object.assign():
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-// referência para Object.keys(): 
+// referência para Object.keys():
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 //referência para Object.values():
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/values
