cc9557384fab7929006ffebcd3feab657162bc31
PedroMarqDev <PedroMarqDev@gmail.com>
2020-10-06 11:21:39 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,8 +17,8 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  for (var counterTwo = 0; counterTwo <= 3; counterTwo += 1) {
+    myArray.push(counterTwo);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
