d4ffd6eb66bea22ba801ba612e1c2c851b09c66a
Demistocles <demi.treze@gmail.com>
2020-10-06 14:07:24 -0300

    correção no terceiro requisito

--- src/vqv.js
+++ src/vqv.js
@@ -21,7 +21,7 @@ const vqv = (nome, idade) => {
     return undefined;
   }
   const phrase = `Oi, meu nome é ${nome}!
-Tenho${idade} anos,
+Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
   return phrase;
