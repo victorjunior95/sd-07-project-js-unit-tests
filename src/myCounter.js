3286745cbfd5972e19b64727273f703a651be88e
Demistocles <demi.treze@gmail.com>
2020-10-05 23:43:20 -0300

    correção

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  const myArray = [];
-  for (let counter = 0; counter < 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (counter = 2; counter < 3; counter += 1) {
-      myArray.push(counter);
+    for (index = 2; counter <= 3; counter += 1) {
+      myArray.push(index);
     }
   }
   return myArray;
