d333bd3f3434803b49c29eb2d23447b0744fe180
Iury Nathan <iurynathan@gmail.com>
2020-10-06 15:57:19 -0300

    corrigindo requisito objCalculator e adicionando productDetails

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -18,20 +18,12 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
-const assert = require("assert")
 
 const calculator = {
   add: (a, b) => a + b,
   mult: (a, b) => a * b,
-  div: (a, b) => {
-    return parseInt(a / b)
-  },
-  sub: (a, b) => a - b
+  div: (a, b) => parseInt(a / b, 10),
+  sub: (a, b) => a - b,
 };
-
-assert.strictEqual(calculator.add(1, 3), 4);
-assert.strictEqual(calculator.mult(10, 3), 30);
-assert.strictEqual(calculator.div(5, 2), 2);
-assert.strictEqual(calculator.sub(1, 3), -2);
 
 module.exports = calculator;
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,15 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(typeof productDetails("Alcool gel", "Máscara"), 'object');
+    assert.strictEqual(Object.keys(productDetails("Alcool gel", "Máscara")).length, 2);
+    assert.strictEqual(typeof productDetails("Alcool gel", "Máscara")[0], "object");
+    assert.strictEqual(typeof productDetails("Alcool gel", "Máscara")[1], "object");
+    assert.notDeepStrictEqual(productDetails("Alcool gel", "Máscara")[0], productDetails("Alcool gel", "Máscara")[1]);
+    assert(
+      productDetails("Alcool gel", "Máscara")[0].details.productId.substr(productDetails("Alcool gel", "Máscara")[0].details.productId.length - 3) === '123'
+      &&
+      productDetails("Alcool gel", "Máscara")[1].details.productId.substr(productDetails("Alcool gel", "Máscara")[1].details.productId.length - 3) === '123'
+    );
   });
 });
