af53f305678ad7ad3d3a4e039c07fd8f133fb791
wellington <profwellington17@gmail.com>
2020-10-05 22:51:50 -0300

    fix atividade 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,11 +14,9 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
- 
-const myCounter = () => {
-  let myArray = [];
-  
-  for (let counter = 0; counter <= 3; counter += 1) {
+ const myCounter = () => {
+  const myArray = [];
+    for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
       myArray.push(counter2);
@@ -27,4 +25,3 @@ const myCounter = () => {
   return myArray;
 };
 module.exports = myCounter;
-
