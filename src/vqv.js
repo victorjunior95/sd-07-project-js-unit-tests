7bb6abd29bd34ac2e9afa4a9d941dd1e0a776a5f
gabrieldeori <deori.pessoal@gmail.com>
2020-10-06 11:23:56 -0300

    #VQV

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let retorno;
+  if (nome !== undefined && idade !== undefined) {
+    retorno = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  } else {
+    retorno = undefined;
+  }
+  return retorno;
+};
 
 module.exports = vqv;
