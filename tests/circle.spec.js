5eaba8a19ec7c75d2a7a584dc45ced6beca8df4b
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-10-06 14:31:07 -0300

    Envio atividades 4.1 4.2 e 4.3

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -26,9 +26,11 @@ const circle = require('../src/circle');
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // Teste se circle retorna um objeto.
-    assert.strictEqual(circle, 'object')
+    assert.strictEqual(typeof (circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(Object.values(circle(1)), " ", undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
