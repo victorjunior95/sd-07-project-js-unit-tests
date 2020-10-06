c886807998decbb86a2a33752bcded32199efbcd
wellington <profwellington17@gmail.com>
2020-10-05 23:46:17 -0300

    fix atividade 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,10 +16,10 @@
 */
 
 const createStudent = (nome) => {
-    const objeto = {
-    name: nome,
-    feedback: () => 'Eita pessoa boa!',
-  };
+  const objeto = {
+      name: nome,
+      feedback: () => 'Eita pessoa boa!',
+    };
   return objeto;
 };
 
