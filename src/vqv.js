0a4d57bbe051d8cde976a6c8a06920da808da707
Matheus Beaumord <matheusbeaumord@gmail.com>
2020-10-06 13:00:23 -0300

    Reparando exercicio VQV

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome == "" && idade === "") return undefined;
+  if (nome === '' && idade === '') return undefined;
   const result = (`Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
-  #VQV!
-  `)
+  #VQV!`)
 };
 
 module.exports = vqv;
