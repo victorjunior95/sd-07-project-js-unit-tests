9df7c114df514e2ff6b388b4f657b4c8b0d48dee
Eriquy Jhordan <eriquy@gmail.com>
2020-10-06 19:43:43 -0300

    function creatStudent is ready

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const student = {};
+  student.name = name;
+  student.feedback = () => 'Eita pessoa boa!';
+  return student;
+};
 
 module.exports = createStudent;
