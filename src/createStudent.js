fc87c43fd7003c23e27544e7a2538634e0aaa67c
richardfazeredo <richardcompletetour@gmail.com>
2020-10-06 21:51:50 -0300

    Requirement 5 - Implementing a function that will be tested by node's assert module

--- src/createStudent.js
+++ src/createStudent.js
@@ -1,29 +1,9 @@
-/*
-  Dada uma função chamada createStudent que recebe como parâmetro um nome,
-  retorne um objeto que contenha duas chaves:
-    (1) name, contendo o nome passado como parâmetro;
-    (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
-
-  Faça a função da chave feedback com arrow functions!
-
-  Parâmetros:
-    - Uma string;
-  Comportamento:
-    const estudante = createStudent('Leandrão, o Lobo Solitário')
-
-    estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
-    estudante.feedback() // Retorna: 'Eita pessoa boa!'
-*/
-
 const createStudent = (name) => {
   const estudante = {
-    name: name,
-    feedback: function feedback() {
-    console.log('Eita pessoa boa!');
-  }};
-  return estudante.feedback();
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
 };
-
-console.log(createStudent('Richard'));
 
 module.exports = createStudent;
