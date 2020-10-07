041e2b17c270eb3ccf68ca7ce3e2934443206252
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-10-07 02:11:10 -0300

    Adição do console.log na createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (nome) => {
   const estudante = {
     name: `${nome}`,
-    feedback: () => { 'Eita pessoa boa!'; },
+    feedback: () => { console.log('Eita pessoa boa!'); },
   };
   return estudante;
 };
