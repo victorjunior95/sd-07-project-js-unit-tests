f15caaa275d539a27cccf471af8c53d405c48047
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-10-07 02:27:06 -0300

    Teste

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (valor1, valor2) => { console.log(Math.trunc(valor1 + valor2)); },
+  mult: (valor1, valor2) => { console.log(Math.trunc(valor1 * valor2)); },
+  div: (valor1, valor2) => { console.log(Math.trunc(valor1 / valor2)); },
+  sub: (valor1, valor2) => { console.log(Math.trunc(valor1 - valor2)); },
+};
 
 module.exports = calculator;
