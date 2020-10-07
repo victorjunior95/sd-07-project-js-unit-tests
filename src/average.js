f6e981aca104b8a644603302059d004021413151
Lets <letsferreiratx@gmail.com>
2020-10-07 11:55:12 -0300

    arrumando erros de average

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (array) => {
   let sum = 0;
   if (array.length === 0) return undefined;
-  for (let index = 0; index <= array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
       return undefined;
     }
