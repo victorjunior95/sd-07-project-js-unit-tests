75cb385ccd8b12e595bd8292c16c8fa87084ac25
paulohp7 <ph.pereira.7@gmail.com>
2020-10-06 22:23:19 -0300

    Fourth objective fixes

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -34,7 +34,11 @@ describe('#circle', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert(circle() === undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert(expected.circumference === 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert(circle(3).area === 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert(circle(3).radius === 3);
+    assert(circle(3).circumference === 18.84);  
   });
 });
