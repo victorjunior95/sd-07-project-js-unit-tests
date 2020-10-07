5a8ecb1d9f2bcb4cde47d5ec918f226a23ef85ce
paulohp7 <ph.pereira.7@gmail.com>
2020-10-06 23:32:51 -0300

    Forth objective assert fixes2

--- src/circle.js
+++ src/circle.js
@@ -23,8 +23,8 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: parseFloat((PI * radius * radius).toFixed(2)),
-    circumference: parseFloat((2 * PI * radius).toFixed(2)),
+    area: parseFloat((PI * radius * radius).toPrecision(4)),
+    circumference: parseFloat((2 * PI * radius).toPrecision(4)),
   };
 };
 
