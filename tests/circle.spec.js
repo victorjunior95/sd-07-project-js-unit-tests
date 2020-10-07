37c77cd07ed2732aa2ebf734ba839e1e437a4d88
MarceloBorges <melombm@hotmail.com>
2020-10-07 05:01:49 -0500

    usado deepStrictEqual para fazer teste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,18 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
+    // deepStrictEqual === Se dois objetos, ou seus objetos filhos, não forem iguais (tanto em valor quanto em tipo), um erro é lançado e o programa é encerrado:
     // Teste se circle retorna um objeto.
+    assert.deepStrictEqual(typeof(circle(3)),'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(Object.keys(circle(3)).length,3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.deepStrictEqual(typeof(circle()), 'undefined');
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.deepStrictEqual(Object.values(circle(2))[2], 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(Object.values(circle(3))[1], 28.259999999999998);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.deepStrictEqual(circle(3), {"area": 28.259999999999998, "circumference": 18.84, "radius": 3});
   });
 });
