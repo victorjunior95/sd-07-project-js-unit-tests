f3f79cfbb703c8fa7b2c1c34bbb89e8ac5e3f115
Victor Junior <victorjr.muchacho@gmail.com>
2020-10-07 12:03:39 -0400

    Desafio 7 remoçao do i em mult+i troca de Math.floor por Math.trunc

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -21,8 +21,8 @@
 
 const calculator = {
   add: (int1, int2) => int1 + int2,
-  multi: (int1, int2) => int1 * int2,
-  div: (int1, int2) => Math.floor(int1 / int2),
+  mult: (int1, int2) => int1 * int2,
+  div: (int1, int2) => Math.trunc(int1 / int2),
   sub: (int1, int2) => int1 - int2,
 };
 
