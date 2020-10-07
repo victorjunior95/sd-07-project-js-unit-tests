a78b733491ee96c7757e24781f506003cb5dae90
LAURO DE SA <itrafor@gmail.com>
2020-10-06 22:30:35 -0300

    Requisito3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome != undefined || idade != undefined) {
+    return `Oi, meu nome é ${nome}!
+    Tenho ${idade} anos,
+    Trabalho na Trybe e mando muito em programação!
+    #VQV`;    
+  }
+  return undefined;
+};
 
 module.exports = vqv;
