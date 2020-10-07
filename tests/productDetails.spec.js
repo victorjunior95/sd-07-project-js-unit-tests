8c6832fef18d09903c1a08dabe4ecf952ea7d4ef
Rafael Fernandes <rafaelsantanafernandes@gmail.com>
2020-10-07 13:12:01 -0300

    autualizando o test product

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,7 +35,7 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.deepStrictEqual(typeof productDetails('gel', 'remedio'), 'array');
+    assert.deepStrictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.deepStrictEqual(Object.keys(productDetails('gel', 'remedio')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
