7eda6925009e2781a36686f287138663ef1eb745
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-10-07 02:05:26 -0300

    correção issue

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,10 +16,11 @@
 */
 
 const createStudent = (nome) => {
-  ({
+  const estudante = {
     name: `${nome}`,
     feedback: () => { 'Eita pessoa boa!'; },
-  });
+  };
+  return estudante;
 };
 
 module.exports = createStudent;
