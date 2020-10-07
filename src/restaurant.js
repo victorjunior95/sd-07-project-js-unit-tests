b8bef8573d54b4c34b6699ecb5958b9ccf03a746
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-10-07 07:52:22 -0300

    explicando pay

--- src/restaurant.js
+++ src/restaurant.js
@@ -5,11 +5,22 @@ const inputConsumption = (str) => {
 };
 const payFunction = (item) => {
   const food = (objRetorno.fetchMenu.food);
+// cria uma constante com o objeto que e proprieda de {obj}...( food:  {*} )
   const drink = (objRetorno.fetchMenu.drink);
+// cria uma constante com o objeto que e proprieda de {obj}...( drink:  {*} )
   const total = Object.assign({}, food, drink);
+// Object.assign cria  um objeto retornado que copia
+// os valores de todas as propriedades dos objetos parametro de obj
+// (food e drink) ;  // (food {*} + drink {*} = {**} )
   for (let i = 0; i < (Object.keys(total).length); i += 1) {
+// Object.keys retorna um array cujos elementos são strings que representam
+// os itens do menu. ex: ( ['coxinha', 'agua', 'coxinha'])
+// o laço varrerá todo esse array.
     if (item === Object.keys(total)[i]) {
       sum += Object.values(total)[i];
+// para cada item adicionado como parametro da funçao  que for estritamente
+// igual a esses elementos (strings do menu) , sera adicionado a soma o valor
+// correspondente a cada um deles..
     }
   }
   return sum;
@@ -21,6 +32,9 @@ const createMenu = (obj) => {
   objRetorno.pay = () => {
     const orderConsumption = objRetorno.consumption;
     orderConsumption.forEach(payFunction);
+// Para cada  item pedido(cada elemento do array objRetorno.consumption),
+// sera realizada a funçao callback payFunction,
+// passando o item como parametro.
     return ((sum * 1.1).toFixed(2)) * 1;
   };
   return objRetorno;
@@ -34,6 +48,8 @@ const createMenu = (obj) => {
 // além de  correções para verificar quaisquer
 // valores (Uso de toFixed() e Multiply by one)
 
+// referencia para forEach:
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 // referência para toFixed():
 // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 // referência para multiply by one:
