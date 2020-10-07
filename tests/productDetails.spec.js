c9da38ef4cf9ae95ed99c79480b88ce68134c44e
MarceloBorges <melombm@hotmail.com>
2020-10-07 05:58:58 -0500

    exercicios produtos details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,18 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(typeof productDetails(),'object');
     // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(productDetails().length, 2);
+    // Teste que os dois itens dentro do array retornado pela função são objetos. link https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+    assert.deepStrictEqual(typeof Object.entries(productDetails), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(Object.keys(productDetails), true); 
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(productDetails('gel', 'gas')[0].details.productId.substr(-3, 3), '123');
+    assert.deepStrictEqual(productDetails('gel', 'gas')[1].details.productId.substr(-3, 3), '123');
   });
 });
