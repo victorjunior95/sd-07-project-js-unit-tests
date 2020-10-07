60e09535531ceadf179d5ffecfced83a5e5c941a
richardfazeredo <richardcompletetour@gmail.com>
2020-10-06 22:47:07 -0300

    Requirement 6 - Implementing tests in a function using node's assert module

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,44 +1,19 @@
-/* eslint-disable max-len*/
-/* eslint-disable no-unused-vars */
-
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
-/*
-  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
-
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(typeof productDetails('Alcool Gel', 'Mascara'), "object");
+    
+    assert.strictEqual(productDetails('Alcool Gel', 'Mascara').length, 2);
+    
+    assert.strictEqual(productDetails('Alcool Gel', 'Mascara')
+    .every(product => typeof product === "object"), true);
+    
+    assert.notDeepStrictEqual(Object.entries(productDetails('a','b')[0], 
+    Object.entries(productDetails('a','b')[1])), true);
+    
+    assert.strictEqual(productDetails('firstProduct', 'secondProduct')
+    .every(product => product.details.productId.endsWith('123')), true);
   });
 });
