610622beef939f23438f3df6c3b21c1efeed22a8
lukeCruz <enio.cruz@yahoo.com.br>
2020-10-07 12:48:29 -0300

    myCounter and objCalculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -7,18 +7,23 @@
   Para cada uma delas atribua uma função que realiza a respectiva operação.
   A função deve receber dois inteiros e retornar um inteiro.
   Os resultados das divisões devem sempre ser arredondados para baixo.
-
   Faça as funções com arrow functions!
-
   Parâmetros:
   - Um número inteiro;
   - Um número inteiro;
-
   Comportamento:
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (primeiroInteiro, segundoInteiro) => primeiroInteiro + segundoInteiro,
+  mult: (primeiroInteiro, segundoInteiro) => primeiroInteiro * segundoInteiro,
+  div: (primeiroInteiro, segundoInteiro) => {
+    const primeiroInteiroAjustado = (primeiroInteiro - (primeiroInteiro % segundoInteiro));
+    return primeiroInteiroAjustado / segundoInteiro;
+  },
+  sub: (primeiroInteiro, segundoInteiro) => primeiroInteiro - segundoInteiro,
+};
 
 module.exports = calculator;
