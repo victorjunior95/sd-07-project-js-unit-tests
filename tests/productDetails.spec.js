9cba04791f0228699b8451d5532a507789001fc1
Victor Junior <victorjr.muchacho@gmail.com>
2020-10-07 11:27:17 -0400

    Desafio 6 trocado valor da matriz do ultimo assert de 0 para 1

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -44,6 +44,6 @@ describe('#productDetails', () => {
     assert.notDeepStrictEqual(productDetails('gel', 'gas')[0], productDetails('gel', 'gas')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
     assert.strictEqual(productDetails('gel', 'gas')[0].details.productId.substr(-3, 3), 123); // com ajuda do projeto de Luciano Berchon
-    assert.strictEqual(productDetails('gel', 'gas')[0].details.productId.substr(-3, 3), 123); // com ajuda do projeto de Luciano Berchon
+    assert.strictEqual(productDetails('gel', 'gas')[1].details.productId.substr(-3, 3), 123); // com ajuda do projeto de Luciano Berchon
   });
 });
