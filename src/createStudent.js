23d0eb02a1226c0bae7d9ee211c0fc94b78a5bf2
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-10-07 02:39:27 -0300

    Adicionando o resultado a uma const

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (nome) => {
   const estudante = {
     name: `${nome}`,
-    feedback: () => { 'Eita pessoa boa!'; },
+    feedback: () => { const phrase = 'Eita pessoa boa!'; return phrase; },
   };
   return estudante;
 };
