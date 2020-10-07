ca2644d33c75d9662a9d16c0d1d24c371f5e59d6
nyxnil <nsmnilson@hotmail.com>
2020-10-07 13:15:53 -0300

    consideracoes em alguns codigos

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,6 +31,29 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+// estava testando com esse codigo
+
+// describe('#productDetails', () => {
+//   it('tests the function has the correct behaviour', () => {
+//     // ESCREVA SEUS TESTES ABAIXO:
+//     // Teste que o retorno da função é um array.
+//     assert.strictEqual(Array.isArray(productDetails()), true);
+//     // Teste que o array retornado pela função contém dois itens dentro.
+//     assert.strictEqual(Object.keys((productDetails()).length), 2);
+//     // Teste que os dois itens dentro do array retornado pela função são objetos.
+//     assert.strictEqual(typeof(productDetails()), 'object');
+//     // Teste que os dois objetos são diferentes entre si.
+//     assert.strictEqual(productDetails()[0] !== productDetails()[1], true);
+//     // (Difícil) Teste que os dois productIds terminam com 123.
+//     assert.deepStrictEqual(productDetails()[0].details.productId.indexOf(123) === (productDetails()[0].details.productId.length - 3), true);
+//     assert.deepStrictEqual(productDetails()[1].details.productId.indexOf(123) === (productDetails()[1].details.productId.length - 3), true);
+//   });
+// });
+
+
+// Verifiquei em vários casos que o meu codigo de cima, foi utilizado da mesma forma
+// Não compreendi o motivo do meu não passar.
+// Codigo utilizado pelo colega Yuri (eu compreendo a forma utilizada, mas prefiro reduzir)
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
