26539ee680cf75d8ea095a4542ea7a6cf3f8d09f
Wendrick <wendrick.b.c1@gmail.com>
2020-10-07 20:34:09 -0300

    testando o 2º

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -36,8 +36,8 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, (2 * 3.14 * 2));
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).area.toFixed(2), 28.26)
+    assert.strictEqual(circle(3).area, (3.14 * 3 * 3));
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepEqual(Object.values(circle(3)), [3, 28.26, 18.84]);
+    assert.deepStrictEqual(Object.values(circle(3)), [3, (3.14 * 3 * 3), (2 * 3.14 * 3)]);
   });
 });
