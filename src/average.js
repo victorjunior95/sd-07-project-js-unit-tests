d1360acf9f6de6134219c1d96130dc2786b2175a
Victor Junior <victorjr.muchacho@gmail.com>
2020-10-07 00:51:58 -0400

    Desafio 1 feito

--- src/average.js
+++ src/average.js
@@ -12,8 +12,16 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (array) => {
   // add your implementation here
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array.length === 0 || typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
