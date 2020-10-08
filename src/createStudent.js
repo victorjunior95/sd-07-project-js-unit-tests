47233269f0c10d7b8817520648573e7396f8a3bd
Wendrick <wendrick.b.c1@gmail.com>
2020-10-07 21:06:36 -0300

    3º questao

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let objeto = {};
+  objeto.name = name;
+  objeto.feedback = () => 'Eita pessoa boa!';
+  return objeto;
+};
 
 module.exports = createStudent;
