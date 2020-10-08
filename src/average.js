0eb4fcb8af7f9eb8effda9c7c6031c792640e262
Damiem <damiemblackwood@gmail.com>
2020-10-08 09:05:01 -0300

    arrumando erros de rules

--- src/average.js
+++ src/average.js
@@ -4,7 +4,7 @@ const average = (array) => {
   }
 
   let sumArray = 0;
-  for (let index = 0; index < array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1){
     if (typeof (array[index]) === 'string') { 
       return undefined;
     }
