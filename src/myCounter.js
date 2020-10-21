3781582d6830b758ce297d71f57f872890afbe7c
Veruska Bittencourt <sousaveruska@gmail.com>
2020-10-21 16:08:28 -0300

    Correçao terceiro commit

--- src/myCounter.js
+++ src/myCounter.js
@@ -18,11 +18,11 @@
 const myCounter = () => {
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);	    myArray.push(counter);
+    myArray.push(counter);	  
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
       myArray.push(counter2);
-    }	    
-  }	  
+    } 
+  }
   return myArray;	
 };
 
