74c52426ccd907990207e10ca1a0ab5979bb7fdf
LAURO DE SA <itrafor@gmail.com>
2020-10-23 11:09:33 -0300

    Requisito 3 - 2310e

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,7 @@
 */
 
 const vqv = (nome, idade) => {
-  if ( nome || idade){
+  if (nome || idade) {
     return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
