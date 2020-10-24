5511861175aeea39c73eec463210b68e2ce03050
LAURO DE SA <itrafor@gmail.com>
2020-10-24 06:21:56 -0300

    Desafio 5 241020a

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const estudent = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudent;
+};
 
 module.exports = createStudent;
