bae7c85965404c7c9c76a984176993d603f93130
ricardo-rals <ricardo.rals.dev@gmail.com>
2020-11-03 10:42:47 -0300

    Update average.js

--- src/average.js
+++ src/average.js
@@ -19,7 +19,7 @@ const average = (array) => {
 
   let sum = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] !== 'number'){
+    if (typeof array[index] !== 'number') {
       return undefined;
     }
     sum += array[index];
