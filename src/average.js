e26d1435867a7efe723e1ef9265303426182ccbb
ricardo-rals <ricardo.rals.dev@gmail.com>
2020-11-03 10:37:46 -0300

    Ajuste no codído do requisito 1

--- src/average.js
+++ src/average.js
@@ -18,7 +18,7 @@ const average = (array) => {
   }
 
   let sum = 0;
-  for (let index = 0 in array) {
+  for(let index = 0; index < array.length; index += 1) {
     if( typeof array[index] !== 'number'){
       return undefined;
     }
