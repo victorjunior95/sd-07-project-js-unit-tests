37d7aae12ed6d160d5cc80cba2bc4a1986fb1e1b
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2020-11-06 10:55:55 -0300

    implementando os testes do productDetails.spec.js

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,9 +36,16 @@ describe('#productDetails', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.ok(typeof productDetails, 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.ok(typeof productDetails()[0], 'object');
+    assert.ok(typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails()[0], productDetails()[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(products[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(products[1].details.productId.endsWith('123'), true);
   });
 });
