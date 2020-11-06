8d49131bd186c17300277c535323780742f722cd
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2020-11-06 11:35:46 -0300

    refatorando para passar no code climate

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let i = 2; i <= 3; i += 1) {
