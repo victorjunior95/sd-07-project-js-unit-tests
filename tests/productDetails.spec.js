5e1b153438aabfa95481a07cf812ed386d20f29f
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2020-11-06 10:56:57 -0300

    apaguei o assert fail

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,7 +33,6 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     assert.ok(typeof productDetails, 'array');
