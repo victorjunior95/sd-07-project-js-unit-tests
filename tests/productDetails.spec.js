8d49e6f2c99bce34287b85a0ac189cb1c75e8859
MatheuzaoCarvalho <matheusvscarvalho@gmail.com>
2020-11-10 15:05:16 -0300

    Adicionando Exercicio - ProductDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,24 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    // assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails(), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    let result = productDetails();
+    let expected = 2;
+    assert.strictEqual(result.length, expected);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof result[0], 'object');
+    assert.strictEqual(typeof result[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    result = productDetails('Alcool', 'Alcool em Gel');
+    assert.notDeepStrictEqual(result[0], result[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    let firstFinalProduct = result[0].details.productId;
+    let secondFinalProduct = result[1].details.productId;
+    assert(firstFinalProduct.endsWith('123'));
+    assert(secondFinalProduct.endsWith('123'));
   });
 });
