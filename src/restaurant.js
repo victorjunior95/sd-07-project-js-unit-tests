fff53ed328222307446b06718e3c3a9f03523f84
nyxnil <nsmnilson@hotmail.com>
2020-11-12 08:57:51 -0300

    saindo da recuperacao

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,6 +71,51 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+let sum = 0;
+const objRetorno = {};
+const inputConsumption = (str) => {
+  objRetorno.consumption.push(str);
+};
+const payFunction = (item) => {
+  const food = (objRetorno.fetchMenu.food);
+// cria uma constante com o objeto que e proprieda de {obj}...( food:  {*} )
+  const drink = (objRetorno.fetchMenu.drink);
+// cria uma constante com o objeto que e proprieda de {obj}...( drink:  {*} )
+  const total = Object.assign({}, food, drink);
+// Object.assign cria  um objeto retornado que copia
+// os valores de todas as propriedades dos objetos parametro de obj
+// (food e drink) ;  // (food {*} + drink {*} = {**} )
+  for (let i = 0; i < (Object.keys(total).length); i += 1) {
+// Object.keys retorna um array cujos elementos são strings que representam
+// os itens do menu. ex: ( ['coxinha', 'agua', 'coxinha'])
+// ( todo objeto e um conjunto de parametros que sao constituidos de chaves, e valores. nesse caso,
+// chave e o item, como coxinha e agua,  e valor e o valor de compra de cada um deles.)
+// o laço varrerá todo esse array.
+    if (item === Object.keys(total)[i]) {
+      sum += Object.values(total)[i];
+// para cada item adicionado como parametro da funçao  que for estritamente
+// igual a esses elementos (strings do menu) , sera adicionado a soma o valor
+// correspondente a cada um deles..
+    }
+  }
+  return sum;
+};
+const createMenu = (obj) => {
+  objRetorno.fetchMenu = obj;
+  objRetorno.consumption = [];
+  objRetorno.order = (inputConsumption);
+  objRetorno.pay = () => {
+    const orderConsumption = objRetorno.consumption;
+    orderConsumption.forEach(payFunction);
+// Para cada  item pedido(cada elemento do array objRetorno.consumption),
+// sera realizada a funçao callback payFunction,
+// passando o item como parametro.
+    return ((sum * 1.1).toFixed(2)) * 1;
+  };
+  return objRetorno;
+};
+
+// referência par ao projeto do Adrian
+// https://github.com/tryber/sd-07-project-js-unit-tests/pull/148/files
 
 module.exports = createMenu;
