dbb2c181223748ea1dd784a75776cb7d4cf23788
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-11-17 17:20:59 -0300

    função recebe [1, 2, '3', 4, 5] e retorna false)

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -24,6 +24,7 @@ describe('#numbers', () => {
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
   });
 });
