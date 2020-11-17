f1295571bd3fdbe35c705d7e6b93c2654cd5ea56
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-11-17 20:12:01 -0300

    remoçao do assert.fail()

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,7 +33,7 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
