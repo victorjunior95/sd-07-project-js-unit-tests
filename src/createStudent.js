d8b844d2f833104da8b34e724e3aab92cede8448
Gustavo Bregalda <gus.bregalda@gmail.com>
2020-11-16 22:19:27 -0300

    alteraçao do item createStudent, para passar no codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  /*return {
+const createStudent = () => {
+  /* return {
     name: nome,
     feedback: () => 'Eita pessoa boa!',
-  };*/
+  }; */
 };
 
 module.exports = createStudent;
