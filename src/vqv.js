928bff3d472932d60e1afa980f8cc89899743504
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-11-17 19:17:02 -0300

    ajustando a frase do arquivo vqv

--- src/vqv.js
+++ src/vqv.js
@@ -21,10 +21,10 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  return (`Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`);
+  return `'Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!'`;
 };
-
+console.log(vqv("Juliana", 44));
 module.exports = vqv;
