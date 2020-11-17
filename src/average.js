cec13112291fad4b443d292a6005eb6f6b145359
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-11-17 16:48:31 -0300

    ajustes requisito 1

--- src/average.js
+++ src/average.js
@@ -19,11 +19,13 @@ const average = (array) => {
 
   let totalSum = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof (array[index]) !== 'number') {
-    return undefined;
     totalSum += array[index];
+
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
     }
-  const endAverage = totalSum / array.length;
+  }
+  const endAverage = Math.round(totalSum / array.length);
   return endAverage;
 };
 
