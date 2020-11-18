6f0cdee6512e6d6b93ad2acbbb96c4e67f39979b
Juliana do Carmo <julianadocarmoti@gmail.com>
2020-11-17 23:03:12 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  const myArray = [];
+  for (let a = 0; a <= 3; a += 1) {
+    myArray.push(a);
+    for (let b = 2; b <= 3; b += 1) {
+      myArray.push(b);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
